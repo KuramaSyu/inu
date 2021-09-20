f930fcfa945c88041ad80fce05a6444e0f201049
KUR4M4 <paul.zenker.pz@gmail.com>
2021-09-20 15:43:20 +0200

    renamed and moved Inu bot class

---
+++ inu/__main__.py
@@ -0,0 +1,33 @@
+import os
+import asyncio
+
+from dotenv import dotenv_values
+import hikari
+import lightbulb
+
+from utils import build_logger
+from core import Inu
+
+log = build_logger()
+
+conf = dotenv_values()
+for key, value in conf.items():
+    print(f"key: {key}\nvalue: {value}")
+print(os.getcwd())
+logs = {
+    "version": 1,
+    "incremental": True,
+    "loggers": {
+        "hikari": {"level": "INFO"},
+        "hikari.ratelimits": {"level": "TRACE_HIKARI"},
+        "lightbulb": {"level": "DEBUG"},
+    },
+}
+
+inu = Inu(
+    prefix="inu-",
+    token=conf["DISCORD_BOT_TOKEN"],
+    intents=hikari.Intents.ALL,
+    logs=logs,
+)
+inu.run()
