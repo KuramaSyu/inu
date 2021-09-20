67b05802e54500610e4c24aa5f6899c48f6571cb
KUR4M4 <paul.zenker.pz@gmail.com>
2021-09-20 15:43:35 +0200

    changed typing

--- inu/ext/prefix/basic_commands.py
+++ inu/ext/prefix/basic_commands.py
@@ -1,11 +1,19 @@
+import typing
+from typing import (
+    Union,
+    Optional,
+    List,
+)
+
 from hikari import embeds
 import lightbulb
 from lightbulb.context import Context
 import hikari
 
+from core import Inu
 
 class Basics(lightbulb.Plugin):
-    def __init__(self, bot: lightbulb.Bot) -> None:
+    def __init__(self, bot: Inu) -> None:
         self.bot = bot
         super().__init__(name="Basic Commands")
 
@@ -17,6 +25,9 @@ class Basics(lightbulb.Plugin):
         embed.description = "hikari built inu is alive"
         await ctx.respond(embed=embed)
 
+    @lightbulb.command()
+    async def test(self, ctx: Context) -> None:
+        await ctx.respond(self.bot.me)
 
 def load(bot):
     bot.add_plugin(Basics(bot))
