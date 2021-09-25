410ba5b2e985a98c4a808a0d15997756997c0b5f
KUR4M4 <paul.zenker.pz@gmail.com>
2021-09-25 02:35:06 +0200

    added help

--- inu/ext/prefix/help.py
+++ inu/ext/prefix/help.py
@@ -1,7 +1,5 @@
-from optparse import Option
-from click import command
-import hikari
-import lightbulb
+import random
+import re
 import typing
 from typing import (
     List,
@@ -10,36 +8,67 @@ from typing import (
     Optional,
     Mapping,
 )
+import hikari
+import lightbulb
+from lightbulb.help import get_command_signature, get_help_text
+from matplotlib.colors import cnames
 
 from core import Inu
+from utils.paginators.h_pag import BasePaginator
 
 T_bot = typing.Union[lightbulb.Bot, Inu]
 
-class Help(lightbulb.help.HelpCommand):
+class CustomHelp(lightbulb.help.HelpCommand):
     def __init__(self, bot: T_bot):
         self.bot = bot
-        self.cmds_per_list: int = 8
+        
         super().__init__(bot=bot)
+
+
+class Help(lightbulb.Plugin):
+    def __init__(self, bot: T_bot):
+        super().__init__(name="Help")
+        self.bot = bot
+        self.cmds_per_list: int = 5
+        self.bot.remove_command("help")
 
     @lightbulb.command()
     async def help(self, ctx: lightbulb.Context, query: Optional[str] = None):
-        pass
+        """
+        Sends help for all or a given query
+        Arguments:
+        [Optional] query: the command you want to search
+        """
+        cmds = self.search(ctx, query)
+        await self.create_help(ctx, cmds)
 
-    async def help_all(self, ctx: lightbulb.Context):
+    def search(
+        self, 
+        ctx: lightbulb.Context, 
+        query: Optional[str] = None
+    ) -> List[lightbulb.Command]:
         cmds: Sequence[lightbulb.Command] = []
         for cmd in self.bot.walk_commands():
-            cmds.append(cmd)
-        if not cmds:
-            return
+            if query == None or query in cmd.qualified_name:
+                cmds.append(cmd)
+        if not cmds and query and len(query) > 1:
+            return self.search(ctx, query[:-1])
+        else:
+            return cmds
+        
+    
+    async def create_help(self, ctx, cmds: List[lightbulb.Command]):
         filtered_cmds = self.filter_hidden(cmds)
         if not filtered_cmds:
             return
-        embeds = await self.create_embed_from(filtered_cmds)
-        await ctx.respond(embed=embeds[-1])
+        embeds = await self.create_embed_from(filtered_cmds, ctx)
+        pag = BasePaginator(page_s=embeds)
+        await pag.start(ctx)
         
-    def filter_hidden(self, 
-    cmds: Sequence[lightbulb.Command]
-    ) -> Sequence[Optional[lightbulb.Command]]:
+    def filter_hidden(
+        self,
+        cmds: Sequence[lightbulb.Command]
+    ) -> Optional[Sequence[lightbulb.Command]]:
         cmds_filtered = []
         for cmd in cmds:
             if cmd.hidden:
@@ -49,27 +78,75 @@ class Help(lightbulb.help.HelpCommand):
 
     async def create_embed_from(
         self,
-        cmds: Sequence[lightbulb.Command]
+        cmds: Sequence[lightbulb.Command],
+        ctx: lightbulb.Context,
     ) -> Sequence[hikari.Embed]:
         pages = []
+        
         for i, cmd in enumerate(cmds):
             if i % self.cmds_per_list == 0:
                 pages.append([])
-            cmd_descr = (
-                f"{cmd.qualified_name}\n"
-                f"```\n{cmd.arg_details}\n```"
+            command_entry = "<....> needed\n[....] optional\n\n"
+            desc = get_help_text(cmd)
+            signature = self.get_command_signature(cmd, ctx)
+            subcommand = ""
+            if isinstance(cmd, lightbulb.commands.Group):
+                subcommand = "Sub commands: " + ", ".join([c.name for c in cmd.walk_commands()]) + "\n"
+            command_entry += (
+                f"{'•' if not subcommand else '⋗'} **{cmd.qualified_name}**\n"
+                f"{subcommand}```\n{signature}\n```\n{desc}\n\n"
             )
-            pages[-1].append(cmd_descr)
+            pages[-1].append(command_entry)
         
         embeds = []
+        colors = [  "orange", "darkorange", "firebrick", "yellowgreen", "limegreen", "mediumturquoise",
+                    "teal", "deepskyblue", "steelblue", "royalblue", "midnightblue",
+                    "slateblue", "blueviolet", "darkviolet", "purple", "crimson"
+        ]
+        def random_color() -> str:
+            return cnames[random.choice(colors)]
+
         for page in pages:
             embed = hikari.Embed()
             embed.title = "Help"
             embed.description = ""
+            embed.color = hikari.Color.from_hex_code(random_color())
             for descr in page:
                 embed.description += f"\n{descr}"
+            embeds.append(embed)
 
         return embeds
+
+    def remove_defaults(self, cmd_signature: str) -> str:
+        start = cmd_signature.find("=")
+        if start == -1:
+            return cmd_signature
+        end = []
+        end.append(cmd_signature.find(">", start))
+        end.append(cmd_signature.find("]", start))
+        while -1 in end:
+            end.remove(-1)
+
+        to_remove = cmd_signature[start:min(end)]
+        cmd_signature = re.sub(to_remove, '', cmd_signature)
+
+        if "=" in cmd_signature:
+            cmd_signature = self.remove_defaults(cmd_signature)
+        return cmd_signature
+
+    def get_command_signature(self, command: lightbulb.Command, ctx: lightbulb.Context):
+        aliases = ", ".join(command.aliases)
+        #cmd_invoke = f"{command.name}({aliases})" if command.aliases else command.name
+        cmd_invoke = f"{command.name}"
+        full_invoke = command.qualified_name.replace(command.name, "")
+        cmd_sign = self.remove_defaults(get_command_signature(command))
+
+        signature = f"{ctx.prefix}{cmd_sign}" #{full_invoke} ..
+        signature += f"\nequal to '{cmd_invoke}': {aliases}" if aliases else ''
+        return signature
+
+                    
+
 
 
 
@@ -78,3 +155,6 @@ class Help(lightbulb.help.HelpCommand):
 
 def load(bot: T_bot):
     bot.add_plugin(Help(bot))
+
+# def unload(bot):
+#     bot.remove_plugin("Help")
