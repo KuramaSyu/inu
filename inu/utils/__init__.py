0d78b446d5fd0876a182903a17b95c9a3547c8a4
KUR4M4 <paul.zenker.pz@gmail.com>
2021-09-27 12:15:11 +0200

    changed paginator structure

--- inu/utils/__init__.py
+++ inu/utils/__init__.py
@@ -1,5 +1,5 @@
 from .logger import build_logger
 from .models import Singleton
-from .paginators.paginator import Paginator
+from .paginators.common import Paginator
 from .string_crumbler import crumble
 from .colors import Color, Colors
