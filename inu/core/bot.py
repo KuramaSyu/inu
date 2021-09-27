e3359c959bd8f2cecfda61f0e51d7b6a831d8a73
KUR4M4 <paul.zenker.pz@gmail.com>
2021-09-27 12:12:52 +0200

    added Configuration class; changed logging

--- inu/core/bot.py
+++ inu/core/bot.py
@@ -5,13 +5,14 @@ import traceback
 import typing
 from typing import (
     Mapping,
-    Union
+    Union,
+    Optional
 )
 import logging
 from utils.logging import LoggingHandler
+
 logging.setLoggerClass(LoggingHandler)
-log = logging.getLogger(__name__)
-log.setLevel(logging.DEBUG)
+
 
 import lightbulb
 import hikari
@@ -21,9 +22,15 @@ from dotenv import dotenv_values
 class Inu(lightbulb.Bot):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.log = logging.getLogger(__name__)
+        self.log.setLevel(logging.DEBUG)
         self.load_prefix()
         self.load_slash()
-        self.conf: Mapping[str, Union[str, None]] = dotenv_values()
+        self.conf: Configuration = Configuration(dotenv_values())
+        self._me: Optional[hikari.User] = None
+        from utils.db import Database
+        self.db = Database(self)
+
 
     @property
     def loop(self) -> asyncio.AbstractEventLoop:
@@ -33,6 +40,8 @@ class Inu(lightbulb.Bot):
 
     @property
     def me(self) -> hikari.User:
+        if self._me:
+            return self._me
         if not (user := self.cache.get_me()):
             raise RuntimeError("Own user can't be accessed from cache")
         return user
@@ -48,7 +57,7 @@ class Inu(lightbulb.Bot):
             try:
                 self.load_extension(f"ext.slash.{extension[:-3]}")
             except Exception as e:
-                log.critical(f"slash command {extension} can't load", exc_info=True)
+                self.log.critical(f"slash command {extension} can't load", exc_info=True)
 
     def load_prefix(self):
         for extension in os.listdir(os.path.join(os.getcwd(), "inu/ext/prefix")):
@@ -60,6 +69,24 @@ class Inu(lightbulb.Bot):
                 continue
             try:
                 self.load_extension(f"ext.prefix.{extension[:-3]}")
-                log.debug(f"loaded plugin: {extension}")
+                self.log.debug(f"loaded plugin: {extension}")
             except Exception as e:
-                log.critical(f"can't load {extension}\n{traceback.format_exc()}", exc_info=True)
+                self.log.critical(f"can't load {extension}\n{traceback.format_exc()}", exc_info=True)
+
+    async def init_db(self):
+        await self.db.connect()
+
+    #override
+    def run(self):
+        super().run()
+
+class Configuration():
+    """Wrapper for the config file"""
+    def __init__(self, config: Mapping[str, Union[str, None]]):
+        self.config = config
+
+    def __getattr__(self, name: str) -> str:
+        result = self.config[name]
+        if result == None:
+            raise AttributeError(f"`Configuration` has no attribute `{name}`")
+        return result
