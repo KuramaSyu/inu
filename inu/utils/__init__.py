21861fc89e15b8687cf7778a7816dc6a6ae33f69
KUR4M4 <paul.zenker.pz@gmail.com>
2021-09-29 15:47:20 +0200

    Update __init__.py

--- inu/utils/__init__.py
+++ inu/utils/__init__.py
@@ -1,5 +1,8 @@
-from .logger import build_logger
+
 from .models import Singleton
 from .paginators.common import Paginator
 from .string_crumbler import crumble
 from .colors import Color, Colors
+from .logging import LoggingHandler
+import logging
+logging.setLoggerClass(LoggingHandler)
