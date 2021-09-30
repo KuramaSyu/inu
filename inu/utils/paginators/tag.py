577d42754a095f24bc06bdda0a07b7abe0b58e5a
KUR4M4 <paul.zenker.pz@gmail.com>
2021-09-30 14:56:31 +0200

    Update tag.py

--- inu/utils/paginators/tag.py
+++ inu/utils/paginators/tag.py
@@ -9,6 +9,8 @@ import asyncio
 
 import hikari
 from hikari import ComponentInteraction, events, ResponseType, Embed
+from hikari.messages import ButtonStyle
+from hikari.impl import ActionRowBuilder
 from .common import (
     Paginator,
     EventListener,
@@ -18,27 +20,63 @@ from .common import (
 
 from utils import crumble
 from utils.tag_mamager import TagManager
+from utils.language import Human
 
 
 class TagHandler(Paginator):
     """An interactive handler for tags"""
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(
+        self,
+        page_s: Union[List[Embed], List[str]],
+        timeout: int = 15*60,
+        component_factory: Callable[[int], ActionRowBuilder] = None,
+        components_factory: Callable[[int], List[ActionRowBuilder]] = None,
+        disable_pagination: bool = False,
+        disable_component: bool = True,
+        disable_components: bool = False,
+        disable_paginator_when_one_site: bool = False,
+    ):
+        super().__init__(
+            page_s=page_s,
+            timeout=timeout,
+            component_factory=component_factory,
+            components_factory=components_factory,
+            disable_pagination=disable_pagination,
+            disable_component=disable_component,
+            disable_components=disable_components,
+            disable_paginator_when_one_site=disable_paginator_when_one_site,  
+        )
         self._name = None
         self._value = None
+        self._options = {
+            "is local": True,
+            "owner": None,
+            "is tag name available": None,
+        }
         self.embed = Embed()
-        self.embed.title = "Creating a tag"
-        self.embed.add_field("Name of your tag", "Not set", inline=False)
-        self.embed.add_field("Value of your tag", "Not set", inline=False)
+        self.embed.title = "Name of your tag (not set)"
+        self.embed.description = "Value of your tag (not set)"
         self._pages = [self.embed]
 
-    async def update_page(self, to_update: str):
-        if to_update == "name":
-            self.embed.edit_field(0, "Name of your tag", f"{self._name or 'Not set'}")
-        elif to_update == "value":
-            values = crumble(str(self._value))
-            self.embed.edit_field(1, "Value of your tag", f"{self._value or 'Not set'}")
-        await self._message.edit(embed=self.embed)
+    async def update_page(self, update_value: bool = False):
+        """to_update: N"""
+        self.embed.title = self._name or "Name your Tag (not set)"
+        if update_value:
+            pages = []
+            for page in crumble(str(self._value), 2048):
+                pages.append(Embed(
+                    title=self._name or "Name of your tag (not set)",
+                    description=page
+                ))
+            self._pages.extend(pages)
+        options = (
+            f"is local: {Human.bool_(self._options['is local'])}"
+        )
+
+        await self._message.edit(
+            embed=self.embed, 
+            components=self.build_default_components(self._position)
+        )
 
 
     @listener(events.InteractionCreateEvent)
@@ -49,8 +87,16 @@ class TagHandler(Paginator):
         if custom_id == "set_name":
             await self.set_name(event.interaction)
         elif custom_id == "set_value" and isinstance(self._value, str):
-
             await self.set_value(event.interaction)
+        elif custom_id == "extend_value":
+            await self.extend_value(event.interaction)
+        elif custom_id == "change_visibility":
+            await self.change_visibility(event.interaction)
+        elif custom_id == "change_owner":
+            pass
+        elif custom_id == "finish":
+            await self.finish()
+
 
     async def set_name(self, interaction: ComponentInteraction):
         embed = Embed(title="Enter a name for your tag:", description=f"You have {self.timeout}s")
@@ -67,10 +113,10 @@ class TagHandler(Paginator):
         except asyncio.TimeoutError:
             return
         self._name = message.content
-        await self.update_page("name")
+        await self.update_page()
 
 
-    async def set_value(self, interaction: ComponentInteraction):
+    async def set_value(self, interaction: ComponentInteraction, append: bool = False):
         embed = Embed(title="Enter the value for your tag:", description=f"You have {self.timeout}s")
         await interaction.create_initial_response(
             ResponseType.MESSAGE_CREATE, 
@@ -84,16 +130,54 @@ class TagHandler(Paginator):
             )
         except asyncio.TimeoutError:
             return
+        if not message.content:
+            return
+        if append and self._name:
+            self._name += message.content
         self._name = message.content
-        await self.update_page("value")
+        await self.update_page(update_value=True)
 
-    async def edit_value(self):
+    async def extend_value(self, interaction: ComponentInteraction):
+        await self.set_value(interaction, True)
+
+    async def change_visibility(self, interaction: ComponentInteraction):
+        if self._options["is local"]:
+            self._options["is local"] = False
+            return
+        self._options["is local"] = True
+
+    async def finish(self):
         pass
 
-    async def edit_title(self):
-        pass
+    def build_default_components(self, position) -> List[ActionRowBuilder]:
+        navi = super().build_default_component(position)
+        tag_specific = (
+            ActionRowBuilder()
+            .add_button(ButtonStyle.PRIMARY, "edit_name")
+            .set_label("edit name")
+            .add_to_container()
+            .add_button(ButtonStyle.PRIMARY, "set value")
+            .set_label("edit value")
+            .add_to_container()
+            .add_button(ButtonStyle.PRIMARY, "extend_value")
+            .set_label("append to value")
+            .add_to_container()
+            .add_button(ButtonStyle.PRIMARY, "change_visibility")
+            .set_label("local/global")
+            .add_to_container()
+            .add_button(ButtonStyle.DANGER, "change_owner")
+            .set_label("change tag owner")
+            .add_to_container()
+        )
+        finish = (
+            ActionRowBuilder()
+            .add_button(ButtonStyle.PRIMARY, "finish")
+            .set_label("Finish")
+            .add_to_container()
+        )
+        if self.pagination:
+            return [navi, tag_specific, finish] #type: ignore
+        return [tag_specific, finish]
 
-    async def exit(self):
-        pass
 
 
