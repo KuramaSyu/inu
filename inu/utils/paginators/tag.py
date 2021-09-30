a387b774800a2dab8fb5351ff3750f5283844eee
KUR4M4 <paul.zenker.pz@gmail.com>
2021-09-30 13:35:52 +0200

    Update tag.py

--- inu/utils/paginators/tag.py
+++ inu/utils/paginators/tag.py
@@ -16,8 +16,12 @@ from .common import (
     listener,
 )
 
+from utils import crumble
+from utils.tag_mamager import TagManager
 
-class TagAddHandler(Paginator):
+
+class TagHandler(Paginator):
+    """An interactive handler for tags"""
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self._name = None
@@ -26,9 +30,15 @@ class TagAddHandler(Paginator):
         self.embed.title = "Creating a tag"
         self.embed.add_field("Name of your tag", "Not set", inline=False)
         self.embed.add_field("Value of your tag", "Not set", inline=False)
-        self._pages = [embed]
+        self._pages = [self.embed]
 
-    def update_page(self):
+    async def update_page(self, to_update: str):
+        if to_update == "name":
+            self.embed.edit_field(0, "Name of your tag", f"{self._name or 'Not set'}")
+        elif to_update == "value":
+            values = crumble(str(self._value))
+            self.embed.edit_field(1, "Value of your tag", f"{self._value or 'Not set'}")
+        await self._message.edit(embed=self.embed)
 
 
     @listener(events.InteractionCreateEvent)
@@ -38,7 +48,8 @@ class TagAddHandler(Paginator):
         custom_id = event.interaction.custom_id or None
         if custom_id == "set_name":
             await self.set_name(event.interaction)
-        elif custom_id == "set_value":
+        elif custom_id == "set_value" and isinstance(self._value, str):
+
             await self.set_value(event.interaction)
 
     async def set_name(self, interaction: ComponentInteraction):
@@ -56,9 +67,33 @@ class TagAddHandler(Paginator):
         except asyncio.TimeoutError:
             return
         self._name = message.content
+        await self.update_page("name")
 
 
     async def set_value(self, interaction: ComponentInteraction):
+        embed = Embed(title="Enter the value for your tag:", description=f"You have {self.timeout}s")
+        await interaction.create_initial_response(
+            ResponseType.MESSAGE_CREATE, 
+            embed=embed
+        )
+        try:
+            message = await self.bot.wait_for(
+                events.MessageCreateEvent, 
+                self.timeout,
+                lambda m: m.author_id == interaction.user.id and m.channel_id == interaction.channel_id
+            )
+        except asyncio.TimeoutError:
+            return
+        self._name = message.content
+        await self.update_page("value")
+
+    async def edit_value(self):
+        pass
+
+    async def edit_title(self):
+        pass
+
+    async def exit(self):
         pass
 
 
