2cce94e0fb512cf6666c9f88269479b819f728bc
KUR4M4 <paul.zenker.pz@gmail.com>
2021-10-01 20:06:22 +0200

    finished tag paginator barely

--- inu/utils/paginators/tag.py
+++ inu/utils/paginators/tag.py
@@ -1,3 +1,4 @@
+import traceback
 import typing
 from typing import (
     Union,
@@ -9,14 +10,14 @@ import asyncio
 import logging
 
 import hikari
-from hikari import ComponentInteraction, events, ResponseType, Embed
+from hikari import ComponentInteraction, InteractionCreateEvent, events, ResponseType, Embed
 from hikari.messages import ButtonStyle
 from hikari.impl import ActionRowBuilder
 import lightbulb
 from lightbulb.converters import WrappedArg
 from lightbulb.converters import user_converter
 
-from inu.utils.tag_mamager import TagIsTakenError
+from utils.tag_mamager import TagIsTakenError
 from .common import (
     Paginator,
     EventListener,
@@ -42,8 +43,8 @@ class Tag():
             - is_stored: (bool) wether or not the tag is already in the db stored
         """
         self.owner: Union[hikari.User, hikari.Member] = owner
-        self._name: Optional[str] = None
-        self._value: Optional[str] = None
+        self.name: Optional[str] = None
+        self.value: Optional[str] = None
         self.is_local_available: bool
         self.is_global_available: bool
         self._is_local: bool = True
@@ -62,26 +63,6 @@ class Tag():
         if not isinstance(self.owner, hikari.Member):
             return None
         return self.owner.guild_id
-
-    @property
-    def value(self) -> str:
-        if not self._value:
-            raise RuntimeError("Can't store a tag without a value")
-        return self.value
-    
-    @value.setter
-    def value(self, value):
-        self._value = value
-
-    @property
-    def name(self) -> str:
-        if not self._value:
-            raise RuntimeError("Can't store a tag without a value")
-        return self.value
-    
-    @name.setter
-    def name(self, value):
-        self._value = value
 
     @property
     def id(self) -> int:
@@ -132,37 +113,41 @@ class NewTagHandler(Paginator):
         """
         self.ctx = ctx
         if not tag:
-            await self.prepare_new_tag(ctx.author)
+            await self.prepare_new_tag(ctx.member or ctx.author)
         else:
-            await self.load_tag(tag, ctx.author)
+            await self.load_tag(tag, ctx.member or ctx.author)
             
         await super().start(ctx)
+
     def options_to_string(self) -> str:
         return (
-            f"global or local: {'local' if self.tag.is_local else 'global'}\n"
+            f"global or local: {'local' if self.tag._is_local else 'global'}\n"
             f"owner: {self.tag.owner.username}\n"
             f"tag stored: {Human.bool_(self.tag.is_stored)}\n"
-            f"tag name local available: {Human.bool_(self.tag.is_local_available, True)}\n"
-            f"tag name global available: {Human.bool_(self.tag.is_global_available, True)}\n"
+            f"tag name local available: {Human.bool_(self.tag.is_local_available)}\n"
+            f"tag name global available: {Human.bool_(self.tag.is_global_available)}\n"
         )
 
     async def update_tag(self) -> None:
-        local_taken, global_taken = await TagManager.is_taken(self._name, self.ctx.guild_id or 0)
+        local_taken, global_taken = await TagManager.is_taken(self.tag.name, self.ctx.guild_id or 0)
         self.tag.owner = self.ctx.author
         self.tag.is_global_available = not global_taken
         self.tag.is_local_available = not global_taken
 
     async def update_page(self, update_value: bool = False):
         """Updates the embed, if the interaction wasn't for pagination"""
-        self.embed.title = self._name or "Name your Tag (not set)"
+        self.embed.title = self.tag.name or "Tag name not set yet"
 
         if update_value:
             pages = []
-            for page in crumble(str(self._value), 2048):
-                pages.append(Embed(
-                    title=self._name or "Name of your tag (not set)",
-                    description=page
-                ))
+            for index, page in enumerate(crumble(str(self.tag.value), 2048)):
+                if index == 0:
+                    self._pages[0].description = self.tag.value or "Tag value not set yet"
+                else:
+                    pages.append(Embed(
+                        title=self.tag.name or "Tag name not set yet",
+                        description=page
+                    ))
             self._pages.extend(pages)
 
         await self.update_tag()
@@ -199,7 +184,7 @@ class NewTagHandler(Paginator):
         elif custom_id == "change_owner":
             await self.change_owner(event.interaction)
         elif custom_id == "finish":
-            await self.finish()
+            await self.finish(event.interaction)
 
 
     async def set_name(self, interaction: ComponentInteraction):
@@ -208,16 +193,19 @@ class NewTagHandler(Paginator):
             ResponseType.MESSAGE_CREATE, 
             embed=embed
         )
+        bot_message = await interaction.fetch_initial_response()
         try:
-            message = await self.bot.wait_for(
+            event = await self.bot.wait_for(
                 events.MessageCreateEvent, 
                 self.timeout,
                 lambda m: m.author_id == interaction.user.id and m.channel_id == interaction.channel_id
             )
         except asyncio.TimeoutError:
             return
-        self._name = message.content
+        self.tag.name = event.message.content
         await self.update_page()
+        if self.ctx.channel:
+            await self.ctx.channel.delete_messages(bot_message, event.message)
 
 
     async def set_value(self, interaction: ComponentInteraction, append: bool = False):
@@ -242,10 +230,10 @@ class NewTagHandler(Paginator):
             return
 
         
-        if append:
-            self._value += event.message.content
+        if append and self.tag.value:
+            self.tag.value += event.message.content
         else:
-            self._value = event.message.content
+            self.tag.value = event.message.content
         await self.update_page(update_value=True)
         if self.ctx.channel:
             await self.ctx.channel.delete_messages(bot_message, event.message)
@@ -254,13 +242,30 @@ class NewTagHandler(Paginator):
         await self.set_value(interaction, append=True)
 
     async def change_visibility(self, interaction: ComponentInteraction):
-        if self.tag.is_local:
+        if self.tag._is_local:
             self.tag._is_local = False
-            return
-        self.tag._is_local = True
+        else:
+            self.tag._is_local = True
+        await interaction.create_initial_response(ResponseType.DEFERRED_MESSAGE_UPDATE)
+        await self.update_page()
 
-    async def finish(self):
-        pass
+    async def finish(self, interaction: ComponentInteraction):
+        try:
+            await self.save()
+        except Exception:
+            tb = traceback.format_exc()
+            pages: List[Embed] = []
+            for page in crumble(tb):
+                embed = Embed()
+                embed.title = "Saving the tag failed"
+                embed.description = page
+                pages.append(embed)
+            return
+        await interaction.create_initial_response(
+            ResponseType.MESSAGE_CREATE,
+            f"Your tag `{self.tag.name}` was successfully added to my storage :)"
+        )
+
 
     async def change_owner(self, interaction: ComponentInteraction):
         embed = Embed(title="Enter the value for your tag:").set_footer(text=f"timeout after {self.timeout}s")
@@ -294,7 +299,7 @@ class NewTagHandler(Paginator):
 
     def build_default_components(self, position) -> List[ActionRowBuilder]:
         navi = super().build_default_component(position)
-        disable_remove_when = lambda self: self._name is None or self._value is None
+        disable_remove_when = lambda self: self.tag.name is None or self.tag.name is None
         tag_specific = (
             ActionRowBuilder()
             .add_button(ButtonStyle.PRIMARY, "set_name")
@@ -309,7 +314,6 @@ class NewTagHandler(Paginator):
             .add_button(ButtonStyle.PRIMARY, "change_visibility")
             .set_label("local/global")
             .add_to_container()
-
         )
         danger_tags = (
             ActionRowBuilder()
@@ -340,7 +344,7 @@ class NewTagHandler(Paginator):
             - author: (Member, User) the user which stored the tag
         """
         guild_id = author.guild_id if isinstance(author, hikari.Member) else 0
-        local_taken, global_taken = await TagManager.is_taken(key=self._name, guild_id = guild_id or 0)
+        local_taken, global_taken = await TagManager.is_taken(key=self.tag.name, guild_id = guild_id or 0)
         new_tag: Tag = Tag(author)
         new_tag.name = tag["tag_key"]
         new_tag.value = tag["key_value"]
@@ -383,17 +387,21 @@ class NewTagHandler(Paginator):
         self.tag = tag
 
         self.embed = Embed()
-        self.embed.title = self.tag.name
-        self.embed.description = self.tag.value
+        self.embed.title = self.tag.name or "Not set"
+        self.embed.description = self.tag.value or "Not set"
         self.embed.add_field(name="Status", value="Unknown - Will be loaded after settig a name")
         self._pages = [self.embed]
 
     async def save(self) -> bool:
         """
-        Saves the current tag
+        Saves the current tag.
+
         Returns:
-            bool: wether successfull or not
+        --------
+            - bool: wether successfull or not
         """
+        if not self.tag.name or not self.tag.value:
+            raise RuntimeError("I can't store a tag without a name and value")
         if self.tag.is_stored:
             try:
                 await TagManager.edit(
