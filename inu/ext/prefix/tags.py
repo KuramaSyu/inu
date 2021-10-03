d6b97e62a69d2bee2e26619db06327e9dddfde0e
KUR4M4 <paul.zenker.pz@gmail.com>
2021-10-03 02:00:35 +0200

    implemented user friendly way to add a tag

--- inu/ext/prefix/tags.py
+++ inu/ext/prefix/tags.py
@@ -19,6 +19,7 @@ from lightbulb.converters import Greedy
 import asyncpg
 
 from core import Inu
+from inu.utils.paginators.tag import TagHandler
 from utils.tag_mamager import TagIsTakenError, TagManager
 from utils import crumble
 from utils.colors import Colors
@@ -36,14 +37,22 @@ class Tags(lightbulb.Plugin):
         super().__init__(name=self.__class__.__name__)
 
     @lightbulb.group()
-    async def tag(self, ctx: Context, key: str):
-        """Get the tag by `key`"""
+    async def tag(self, ctx: Context, *, key: str = None):
+        """Get the tag by `key`
+        
+        Args:
+        ----
+            - key: the name of the tag
+                - if `key` isn't provided I'll start an interactive tag creation menu
+        """
+        if key is None:
+            taghandler = TagHandler()
+            return await taghandler.start(ctx)
+
         records = await TagManager.get(key, ctx.guild_id or 0)
         record: Optional[asyncpg.Record] = None
         # if records are > 1 return the local overridden one
         if len(records) >= 1:
-            print(">1")
-            print(records)
             typing.cast(int, ctx.guild_id)
             for r in records:
                 if r["guild_id"] == ctx.guild_id:
@@ -54,8 +63,6 @@ class Tags(lightbulb.Plugin):
         if record is None:
             return await ctx.respond("I can't found a tag named `{key}` in my storage")
         messages = []
-
-        print(record)
         for value in crumble("\n".join(v for v in record["tag_value"])):
             message = f"**{key}**\n\n{value}\n\n`created by {self.bot.cache.get_user(record['creator_id']).username}`"
             messages.append(message)
@@ -63,77 +70,75 @@ class Tags(lightbulb.Plugin):
         await pag.start(ctx)
 
     @tag.command()
-    async def add(self, ctx: Context, key: str, *, value: str):
-        """Add a tag"""
-        print(value)
-        if value == None:
-            return
+    async def add(self, ctx: Context, key: str = None, *, value: str = None):
+        """Add a tag to my storage
+        
+        Args:
+        -----
+            - key: the name the tag should have
+            NOTE: the key is the first word you type in! Not more and not less!!!
+            - value: that what the tag should return when you type in the name. The value is all after the fist word
+        """
+        if value is None or key is None:
+            taghandler = TagHandler()
+            return await taghandler.start(ctx)
         typing.cast(str, value)
         try:
             await TagManager.set(key, value, ctx.member or ctx.author)
         except TagIsTakenError:
             return await ctx.respond("Your tag is already taken")
         return await ctx.respond(f"Your tag `{key}` has been added to my storage")
-        
-
-
-
-    @tag.command(aliases=["del"])
-    async def remove(self, key):
-        pass
 
     @tag.command()
-    async def edit(self, key):
+    async def edit(self, ctx: Context, key: str):
+        """Add a tag to my storage
+        
+        Args:
+        -----
+            - key: the name the tag should have
+            NOTE: the key is the first word you type in! Not more and not less!!!
+            - value: that what the tag should return when you type in the name. The value is all after the fist word
+        """
+        results: List[asyncpg.Record] = await TagManager.get(key)
+        if len(results) == 0:
+            return await ctx.respond(f"I can't find a tag with the name `{key}` in my storage :/")
+        elif len(results) == 1:
+            taghandler = TagHandler()
+            return await taghandler.start(ctx, results[0])
+        else:
+            #  select between global and local - needs to lookup the results if there are tags of author
+
+            menu = (
+                ActionRowBuilder()
+                .add_select_menu("menu")
+                .add_option(f"{key} - global", ""
+                )
+
+            )
+        # selection menu here
+            
+        
+    @tag.command()
+    async def remove(self, ctx: Context, key: str = None):
+        """NOT IMPLEMENTED YET! IGNORE: Add a tag to my storage
+        
+        Args:
+        -----
+            - key: the name the tag should have
+            NOTE: the key is the first word you type in! Not more and not less!!!
+            - value: that what the tag should return when you type in the name. The value is all after the fist word
+        """
         pass
+        # if value is None or key is None:
+        #     taghandler = TagHandler()
+        #     return await taghandler.start(ctx)
+        # typing.cast(str, value)
+        # try:
+        #     await TagManager.set(key, value, ctx.member or ctx.author)
+        # except TagIsTakenError:
+        #     return await ctx.respond("Your tag is already taken")
+        # return await ctx.respond(f"Your tag `{key}` has been added to my storage")
 
-
-    def tag_embed_builder(self) -> List[Embed]:
-        start_page = hikari.Embed()
-        start_page.title = "Select what you want to do"
-
-    def components_builder(self, position: int) -> List[ActionRowBuilder]:
-        start_page = [(
-            ActionRowBuilder()
-            .add_button(ButtonStyle.PRIMARY, "add_tag")
-            .set_label("Add tag")
-            .add_to_container()
-            .add_button(ButtonStyle.PRIMARY, "edit_tag")
-            .set_label("Edit tag")
-            .add_to_container()
-            .add_button(ButtonStyle.DANGER, "remove_tag")
-            .set_label("Remove tag")
-            .add_to_container()
-        ), navigation_row(position, compact=True, len_pages=0)]
-        add_tag = [(
-            ActionRowBuilder()
-            .add_button(ButtonStyle.PRIMARY, "set_key")
-            .set_label("Set a name")
-            .add_to_container()
-            .add_button(ButtonStyle.PRIMARY, "edit_tag")
-            .set_label("Edit tag")
-            .add_to_container()
-            .add_button(ButtonStyle.DANGER, "remove_tag")
-            .set_label("Remove tag")
-            .add_to_container()
-        ), navigation_row(position, compact=True, len_pages=0)]
-        edit_tag = [(
-            ActionRowBuilder()
-            .add_button(ButtonStyle.PRIMARY, "remove_tag")
-            .set_label("Remove tag")
-            .add_to_container()
-            .add_button(ButtonStyle.PRIMARY, "rename_tag")
-            .set_label("Rename tag")
-            .add_to_container()
-            .add_button(ButtonStyle.DANGER, "change_owner")
-            .set_label("Change owner")
-            .add_to_container()
-            .add_button(ButtonStyle.PRIMARY, "tag_set")
-            .set_label("new value")
-            .add_to_container()
-            .add_button(ButtonStyle.PRIMARY, "tag_append")
-            .set_label("extend Value")
-            .add_to_container()
-        )]
 
     async def tag_add_i(self, ctx):
         pass
