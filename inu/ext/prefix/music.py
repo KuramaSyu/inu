14938af4cb9696c4d15cf2113c10cab767e22969
KUR4M4 <paul.zenker.pz@gmail.com>
2021-11-05 21:06:29 +0100

    Update music.py

--- inu/ext/prefix/music.py
+++ inu/ext/prefix/music.py
@@ -1,4 +1,5 @@
 import os
+import traceback
 import typing
 from typing import (
     Optional,
@@ -10,9 +11,11 @@ from typing import (
 import asyncio
 import logging
 import asyncio
+import datetime
+from pprint import pformat
 
 import hikari
-from hikari import ComponentInteraction, Embed, ShardReadyEvent
+from hikari import ComponentInteraction, Embed, ResponseType, ShardReadyEvent, VoiceState, VoiceStateUpdateEvent
 from hikari.impl import ActionRowBuilder
 import lightbulb
 from lightbulb import Context, command, check
@@ -25,7 +28,7 @@ log.setLevel(logging.DEBUG)
 
 
 # If True connect to voice with the hikari gateway instead of lavasnek_rs's
-HIKARI_VOICE = True
+HIKARI_VOICE = False
 
 
 class EventHandler:
@@ -53,6 +56,7 @@ class Interactive:
     def __init__(self, bot: Inu):
         self.bot = bot
         self.lavalink = self.bot.data.lavalink
+        self.queue_msg: Optional[hikari.Message] = None
 
 
     async def ask_for_song(self, ctx: lightbulb.Context, query: str, displayed_song_count: int = 3) -> Optional[lavasnek_rs.Track]:
@@ -69,8 +73,6 @@ class Interactive:
         -------
             - (lavasnek_rs.Track | None) the chosen title (is None if timeout or other errors)
         """
-        # Search the query, auto_search will get the track from a url if possible, otherwise,
-        # it will search the query on youtube.
         if not ctx.guild_id:
             return
         query_print = ""
@@ -85,12 +87,12 @@ class Interactive:
                 track = query_information.tracks[x]
             except IndexError:
                 break
-            query_print = f"{x+1} | [{track.info.title}]({track.info.uri})\n"
+            query_print = f"{x+1} | {track.info.title}"
             if len(query_print) > 100:
                 query_print = query_print[:100]
-            menu.add_option(query_print, "x").add_to_menu()
+            menu.add_option(query_print, str(x)).add_to_menu()
 
-        menu.add_to_container()
+        menu = menu.add_to_container()
         menu_msg = await ctx.respond(f"Choose the song which should be added", component=menu)
 
         try:
@@ -107,8 +109,13 @@ class Interactive:
                 return  # to avoid problems with typecheckers
             track_num = int(event.interaction.values[0])
         except asyncio.TimeoutError as e:
-            raise e
-        
+            return None
+        log.debug(f"track num: {track_num}")
+        await event.interaction.create_initial_response(
+            ResponseType.DEFERRED_MESSAGE_UPDATE
+        )
+        log.debug(pformat([track.info.title for track in query_information.tracks]))
+        log.debug(query_information.tracks[track_num].info.title)
         return query_information.tracks[track_num]
         # await self.lavalink.play(ctx.guild_id, query_information.tracks[track_num]).requester(ctx.author.id).queue()
         # music_embed = hikari.Embed()
@@ -140,12 +147,28 @@ class Music(lightbulb.Plugin):
         self.bot = bot
         self.log = logging.getLogger(__name__)
         self.log.setLevel(logging.DEBUG)
+        self.interactive = Interactive(self.bot)
+        self.music_message = {}  # guild_id: hikari.Message
 
     @lightbulb.listener(hikari.ShardReadyEvent)
     async def on_ready(self, event: ShardReadyEvent):
-        self.lavalink = self.bot.data.lavalink
+        pass
 
-    # @lightbulb.check(lightbulb.guild_only) #type: ignore
+        self.log.info("Lavalink is connected")
+
+    @lightbulb.listener(hikari.VoiceStateUpdateEvent)
+    async def on_voice_state_update(self, event: VoiceStateUpdateEvent):
+        # check if the user is the bot
+        if not event.state.user_id == self.bot.get_me().id: # type: ignore
+            return
+
+        # bot connected (No channel -> channel)
+        if event.old_state is None and event.state.channel_id:
+            pass
+        elif event.state.channel_id is None and not event.old_state is None:
+            await self._leave(event.guild_id)
+
+
     async def _join(self, ctx: lightbulb.Context) -> Optional[hikari.Snowflake]:
         if not (guild := ctx.get_guild()) or not ctx.guild_id:
             return
@@ -188,9 +211,10 @@ class Music(lightbulb.Plugin):
             builder.set_start_gateway(False)
 
         lava_client = await builder.build(EventHandler())
-
         self.bot.data.lavalink = lava_client
-        logging.debug("lavalink loaded")
+        self.lavalink = self.interactive.lavalink = self.bot.data.lavalink
+        log.debug(type(self.interactive.lavalink))
+        logging.info("lavalink is connected")
 
     @lightbulb.check(lightbulb.guild_only)
     @lightbulb.command()
@@ -205,50 +229,121 @@ class Music(lightbulb.Plugin):
     @lightbulb.command()
     async def leave(self, ctx: lightbulb.Context) -> None:
         """Leaves the voice channel the bot is in, clearing the queue."""
-
-        await self.bot.data.lavalink.destroy(ctx.guild_id)
-
-        if HIKARI_VOICE:
-            await self.bot.update_voice_state(ctx.guild_id, None)
-            await self.bot.data.lavalink.wait_for_connection_info_remove(ctx.guild_id)
-        else:
-            await self.bot.data.lavalink.leave(ctx.guild_id)
-
-        # Destroy nor leave remove the node nor the queue loop, you should do this manually.
-        await self.bot.data.lavalink.remove_guild_node(ctx.guild_id)
-        await self.bot.data.lavalink.remove_guild_from_loops(ctx.guild_id)
-
+        if not ctx.guild_id:
+            return  # just for pylance
+        await self._leave(ctx.guild_id)
         await ctx.respond("Left voice channel")
 
+    async def _leave(self, guild_id: int):
+        await self.bot.data.lavalink.destroy(guild_id)
+
+        if HIKARI_VOICE:
+            await self.bot.update_voice_state(guild_id, None)
+            await self.bot.data.lavalink.wait_for_connection_info_remove(guild_id)
+        else:
+            await self.bot.data.lavalink.leave(guild_id)
+
+        # Destroy nor leave remove the node nor the queue loop, you should do this manually.
+        await self.bot.data.lavalink.remove_guild_node(guild_id)
+        await self.bot.data.lavalink.remove_guild_from_loops(guild_id)
+        
     @lightbulb.check(lightbulb.guild_only)
     @lightbulb.command()
     async def play(self, ctx: lightbulb.Context, *, query: str) -> None:
         """Searches the query on youtube, or adds the URL to the queue."""
-
+        if not ctx.guild_id or not ctx.member:
+            return  # just for pylance
         con = await self.bot.data.lavalink.get_guild_gateway_connection_info(ctx.guild_id)
         # Join the user's voice channel if the bot is not in one.
         if not con:
             await self._join(ctx)
 
-        # Search the query, auto_search will get the track from a url if possible, otherwise,
-        # it will search the query on youtube.
-        query_information = await self.bot.data.lavalink.auto_search_tracks(query)
+        # check for youtube playlist
+        if 'youtube' in query and 'playlist?list=' in query:
+            await self.load_yt_playlist(ctx, query)
+        else:
+            track = await self.search_track(ctx, query)
+            if track is None:
+                await ctx.respond("I can't find anything with your query")
+                return
+            await self.load_track(ctx, track)
 
-        if not query_information.tracks:  # tracks is empty
-            await ctx.respond("Could not find any video of the search query.")
-            return
 
+        # play the track
+        
+
+
+        self.log.debug("start queue()")
+        await self.queue(ctx)
+        self.log.debug("ended queue()")
+
+    async def load_track(self, ctx: Context, track: lavasnek_rs.Track):
+        guild_id = ctx.guild_id
+        author_id = ctx.author.id
+        if not guild_id:
+            raise Exception("guild_id is missing in `lightbulb.Context`")
         try:
-            # `.requester()` To set who requested the track, so you can show it on now-playing or queue.
             # `.queue()` To add the track to the queue rather than starting to play the track now.
-            await self.bot.data.lavalink.play(ctx.guild_id, query_information.tracks[0]).requester(
-                ctx.author.id
+            await self.bot.data.lavalink.play(guild_id, track).requester(
+                author_id
             ).queue()
         except lavasnek_rs.NoSessionPresent:
             await ctx.respond(f"Use `{self.bot.conf.DEFAULT_PREFIX}join` first")
             return
 
-        await ctx.respond(f"Added to queue: {query_information.tracks[0].info.title}")
+        embed = Embed(
+            title=f'Title added',
+            description=f'[{track.info.title}]({track.info.uri})'
+        ).set_thumbnail(ctx.member.avatar_url)  # type: ignore
+        await ctx.respond(embed=embed)
+
+    async def load_yt_playlist(self, ctx: Context, query) -> lavasnek_rs.Tracks:
+        """
+        loads a youtube playlist
+
+        Returns
+        -------
+            - (lavasnek_rs.Track) the first track of the playlist
+        """
+        tracks = await self.lavalink.get_tracks(query)
+        log.debug(len(tracks.tracks))
+        for track in tracks.tracks:
+            await self.bot.data.lavalink.play(ctx.guild_id, track).requester(
+                ctx.author.id
+            ).queue()
+        if tracks.playlist_info:
+            embed = Embed(
+                title=f'Playlist added',
+                description=f'[{tracks.playlist_info.name}]({query})'
+            ).set_thumbnail(ctx.member.avatar_url)
+            await ctx.respond(embed=embed)
+        return tracks
+
+    async def search_track(self, ctx: Context, query: str) -> Optional[lavasnek_rs.Track]:
+        """
+        searches the query and returns the Track or None
+        """
+        query_information = await self.bot.data.lavalink.auto_search_tracks(query)
+        track = None
+        if not query_information.tracks:  # tracks is empty
+            await ctx.respond("Could not find any video of the search query.")
+            return None
+
+        if len(query_information.tracks) > 1:
+            try:
+                track = await self.interactive.ask_for_song(ctx, query)
+                if track is None:
+                    self.log.warning("track is None - error")
+            except Exception:
+                self.log.error(traceback.print_exc())
+                await ctx.respond("Error")
+
+        else:
+            track = query_information.tracks[0]
+
+        if track is None:
+            return None
+        return track
 
     @lightbulb.check(lightbulb.guild_only)
     @lightbulb.command()
@@ -264,7 +359,8 @@ class Music(lightbulb.Plugin):
         """Skips the current song."""
 
         skip = await self.bot.data.lavalink.skip(ctx.guild_id)
-        node = await self.bot.data.lavalink.get_guild_node(ctx.guild_id)
+        if not (node := await self.bot.data.lavalink.get_guild_node(ctx.guild_id)):
+            return
 
         if not skip:
             await ctx.respond("Nothing to skip")
@@ -341,6 +437,122 @@ class Music(lightbulb.Plugin):
             await self.bot.data.lavalink.raw_handle_event_voice_server_update(
                 event.guild_id, event.endpoint, event.token
             )
+
+    async def queue(self, ctx: Context):
+        '''
+        refreshes the queue of the player
+        '''
+        if not ctx.guild_id:
+            return
+        channel = ctx.get_channel()
+        node = await self.bot.data.lavalink.get_guild_node(ctx.guild_id)
+        if not node:
+            self.log.warning("node is None, in queue command")
+            return
+        numbers = ['1️⃣','2️⃣','3️⃣','4️⃣','5️⃣','6️⃣','7️⃣','8️⃣','9️⃣','🔟']
+        upcoming_songs = ''
+        for track in node.queue:
+            self.log.debug(track.track.info.title)
+        for x in range (1,5,1):
+            try:
+                num = numbers[int(x) - 1]
+                upcoming_songs = (
+                    f'{upcoming_songs}\n' 
+                    f'{num} {str(datetime.timedelta(milliseconds=int(int(node.queue[x - 1].track.info.length))))} '
+                    f'- {node.queue[x - 1].track.info.title}'
+                )
+            except:
+                break
+        queue = None
+        if upcoming_songs == '':
+            upcoming_songs = '/'
+        elif int(len(node.queue)) > 4:
+            queue_len = int(len(node.queue))-4
+            if queue_len > 1:
+                queue = f'waiting in Queue: ---{queue_len}--- songs'
+            else:
+                try:
+                    queue = f'waiting in Queue: ---{queue_len}--- song'
+                except:
+                    queue = f'waiting in Queue: ---N0TH1NG---'
+        if queue is None:
+            queue = f'Queue: ---N0TH1NG---'
+        try:
+            track = node.now_playing.track
+        except Exception:
+            return self.log.warning("can't get current playing song")
+
+        if not node.now_playing.requester:
+            self.log.warning("no requester of current track - returning")
+        #get thumbnail of the video
+
+        self.log.debug(track.info.uri)
+        requester = ctx.get_guild().get_member(int(node.now_playing.requester))
+        current_duration = str(datetime.timedelta(milliseconds=int(int(track.info.length))))
+        music_embed = hikari.Embed(
+            colour=hikari.Color.from_rgb(71, 89, 211)
+        )
+        music_embed.add_field(name = "Playing Song:", value=f'[{track.info.title}]({track.info.uri})', inline=True)#{"🔂 " if player.repeat else ""}
+        music_embed.add_field(name = "Author:", value=f'{track.info.author}', inline=True)
+        music_embed.add_field(name="Added from:", value=f'{requester.display_name}' , inline=True)
+        music_embed.add_field(name = "Duration:", value=f'{current_duration}', inline=False)
+        
+        
+        
+        # music_embed.set_thumbnail(url=f'{video_thumbnail}')
+        music_embed.add_field(name = "——————————Queue—————————————————", value=f'```ml\n{upcoming_songs}\n```', inline=False)
+        music_embed.set_footer(text = f'{queue or "/"}')
+        try:
+            self.music_message[ctx.guild_id]
+        except:
+            self.music_message[ctx.guild_id] = None
+
+        #edit existing message
+        resume = True
+        if self.music_message[ctx.guild_id] != None:
+            try:
+                timeout = 4
+                async for m in self.bot.rest.fetch_messages(ctx.channel_id):
+                    if m.id == self.music_message[ctx.guild_id].id:
+                        await self.music_message[ctx.guild_id].edit(embed=music_embed)
+                        resume = False
+                    timeout -= 1
+                    if timeout == 0:
+                        break
+            except Exception as e:
+                traceback.print_exc()
+        try:
+            if resume:
+                #last message not among the last 3. Del and re send
+                if self.music_message[ctx.guild_id] != None:
+                    await self.music_message[ctx.guild_id].delete()
+                    self.music_message[ctx.guild_id] = None#edit(embed=music_embed)
+                    self.music_message[ctx.guild_id] = await ctx.respond(embed=music_embed)
+                    message = self.music_message[ctx.guild_id]
+                    await self.add_music_reactions(message)
+                else: #self.music_message[str(ctx.guild)] == None:
+                    self.music_message[ctx.guild_id] = await ctx.respond(embed=music_embed)
+                    message = self.music_message[ctx.guild_id]
+                    await self.add_music_reactions(message)
+        except Exception as e:
+            traceback.print_exc()
+            try:
+                self.music_message[ctx.guild_id] = await ctx.respond(embed=music_embed)
+                message = self.music_message[ctx.guild_id]
+                await self.add_music_reactions(message)
+            except Exception as e:
+                traceback.print_exc()
+        return
+
+    async def add_music_reactions(self, message: hikari.Message):
+        await message.add_reaction(str('1️⃣'))
+        await message.add_reaction(str('2️⃣'))
+        await message.add_reaction(str('3️⃣'))
+        await message.add_reaction(str('4️⃣'))
+        await message.add_reaction(str('🔀'))
+        await message.add_reaction(str('🗑'))
+        await message.add_reaction(str('🛑'))
+        await message.add_reaction(str('⏸'))
 
 
 def load(bot: Inu) -> None:
