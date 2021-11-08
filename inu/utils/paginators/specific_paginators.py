319c9ee7e1a8d5929480807d728fd4ec13111ca8
KUR4M4 <paul.zenker.pz@gmail.com>
2021-11-08 15:54:08 +0100

    override build_default_compponents to add the selection menu

--- inu/utils/paginators/specific_paginators.py
+++ inu/utils/paginators/specific_paginators.py
@@ -1,9 +1,11 @@
+from argparse import Action
 from types import TracebackType
 import discord
 from typing import Union, Optional, List, Dict
 import traceback
 
 import hikari
+from hikari.impl import ActionRowBuilder
 import lightbulb
 
 from .common import PaginatorReadyEvent
@@ -17,6 +19,7 @@ class MusicHistoryPaginator(Paginator):
         *,
         history: List[Dict[str, str]],
         pages: Union[List[hikari.Embed], List[str]],
+        items_per_site: int,
         timeout: int = 60,
     ):
         super().__init__(
@@ -25,7 +28,24 @@ class MusicHistoryPaginator(Paginator):
         )
         self.not_valid = 0
         self.song_list = history
+        self.items_per_site = items_per_site
     
+    def build_default_components(self, position: int):
+        components = [self.build_default_component(position)]
+        start = self._position * self.items_per_site
+        menu = (
+            ActionRowBuilder()
+            .add_select_menu("history menu")
+        )
+        for x in range(self.items_per_site):
+            menu.add_option(
+                f"{x+start} | {self.song_list[x+start]['title']}",
+                str(int(x+start))
+            ).add_to_menu()
+        menu = menu.add_to_container()
+        components.append(menu)
+        return components
+
     @listener(PaginatorReadyEvent)
     async def on_start(self):
         print("on_start")
@@ -78,11 +98,18 @@ class MusicHistoryPaginator(Paginator):
             text=f"{len(numbers)} {'track is' if len(numbers) <= 1 else 'tracks are'} "\
                  f"added by {self.ctx.member.display_name}"
         )
+        menu = (
+            ActionRowBuilder()
+            .add_select_menu("music menu")
+        )
         for num in numbers:
             d = self.song_list[num]
             embed.description += f"{num} | [{d['title']}]({d['uri']})\n"
+            menu = menu.add_option(f"{num} | {d['title']}", str(num)).add_to_menu()
             links.append(d["uri"])
-        await self.ctx.respond²(embed=embed)
+        embed.set_author(name="Enter number(s) OR select in the menu")
+        menu.add_to_container()
+        await self.ctx.respond(embed=embed, component=menu)
         await self.play(self.ctx, query=links)
 
 
