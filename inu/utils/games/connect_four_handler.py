f65fb87089ad8ba06fbb015c93fe6cd924ab07f0
KUR4M4 <paul.zenker.pz@gmail.com>
2021-12-04 04:18:08 +0100

    Update connect_four_handler.py

--- inu/utils/games/connect_four_handler.py
+++ inu/utils/games/connect_four_handler.py
@@ -53,7 +53,7 @@ class HikariPlayer(Player):
     def __init__(
         self,
         name: str,
-        id: str,
+        id: int,
         token: str,
         user: hikari.Member
     ):
@@ -104,6 +104,10 @@ class Board:
             else:
                 break
         self.board[first_free_slot][column] = player
+    class GameOverCoordinate:
+        def __init__(self, row: int, column: int):
+            self.row = row
+            self.col = column
         
     def check_for_game_over(self):
         """
@@ -114,15 +118,25 @@ class Board:
         def check(lines: List) -> Tuple[int, int]:
             for i_l, line in enumerate(lines):
                 count = 0
+                id = 0
                 for i_s, slot in enumerate(line):
                     if count >= 4:
                         return i_l, i_s-1  #(in the last iteration it got 4, hence -1)
+                    if slot is None:
+                        count = 0
+                        continue
+                    if id != slot.id:
+                        count = 0
+                        id = slot.id
+                    count += 1
+                            
+                    
             return -1, -1
-        line, slot = check(*self.board)
+        line, slot = check([*self.board])
         if not -1 in [line, slot]:
             self.game_over_coordinates = [(line, slot), (line, slot-1), (line, slot-2), (line, slot-3)]
             return
-        line, slot = check([*Grid.get_cols(self.board)])
+        slot, line = check([*Grid.get_cols(self.board)])
         if not -1 in [line, slot]:
             self.game_over_coordinates = [(line, slot), (line-1, slot), (line-2, slot), (line-3, slot)]
             return
@@ -135,18 +149,19 @@ class Board:
         part = ""
         string = ""
         for line in self.board:
-            for player in line:
-                if player:
-                    part += player.token
+            for slot in line:
+                if slot:
+                    part += slot.token
                 else:
                     part += self.bg_color
             lines.append(part)
+            part = ""
 
         if not self.game_over_coordinates:        
             return "\n".join(lines)
 
         for c in self.game_over_coordinates:
-            lines[c[0]][c[1]] = self.marker
+            lines[c[0]] = f"{lines[c[0]][:c[1]]}{self.marker}{lines[c[0]][c[1]+1:]}"
         return "\n".join(lines)
                     
                 
@@ -167,6 +182,7 @@ class BaseConnect4:
         self.game_winner: Optional[Player] = None
         self.game_over = True
         self.board = Board(extra_marker)
+        self.start()
         
     def start(self):
         self.game_over = False
@@ -221,11 +237,14 @@ class Connect4Handler(Paginator):
         self.player1 = player1
         self.player2 = player2
         super().__init__(
+            self=self,
             page_s=["starting up..."],
             timeout=10*60,
             disable_pagination=True,
+            disable_component=True,
+            disable_components=True,
             disable_paginator_when_one_site=False,
-            listen_to_events=[hikari.ReactionAddEvent]
+            listen_to_events=[hikari.GuildReactionAddEvent]
         )
         # generate player marks
         marks = ['🔵','🟣','🔴','🟠','🟢','🟡','🟤'] #,'⚪'
@@ -239,11 +258,11 @@ class Connect4Handler(Paginator):
         extra_marker = random.choice(marks)
 
         self.orienation_letter = [f':regional_indicator_{l}:' for l in 'abcdefgh']
-        self.orientation_number = "1️⃣2️⃣3️⃣4️⃣5️⃣6️⃣7️⃣8️⃣"  #*️⃣ #*️⃣
+        self.orientation_number = ["1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣", "6️⃣", "7️⃣", "8️⃣"]  #*️⃣ #*️⃣
 
         # generate game
-        g_player1 = HikariPlayer(player1.display_name, player1.id, self.mark1, player1)
-        g_player2 = HikariPlayer(player2.display_name, player2.id, self.mark2, player2)
+        g_player1 = HikariPlayer(player1.display_name, 1, self.mark1, player1)
+        g_player2 = HikariPlayer(player2.display_name, 2, self.mark2, player2)
         self.game = BaseConnect4(g_player1, g_player2, extra_marker)
         
         self.game_infos = []
@@ -253,25 +272,90 @@ class Connect4Handler(Paginator):
     
     @listener(PaginatorReadyEvent)
     async def build_up_game(self, _: PaginatorReadyEvent):
-        pass
+        await self.update_embed()
+        for emoji in self.orientation_number:
+            await self._message.add_reaction(emoji)
+        log.debug("return from ready")
+    async def update_embed(self):
+        log.debug("update message")
+        await self._message.edit(embed=self.build_embed())
     
-    def update_embed(self):
+    def build_embed(self):
         embed = hikari.Embed()
         embed.title = f"{'- '*4} Connect 4 {'- '*4}"
         embed.description = self.board_to_str()
         if self.game_infos:
-            embed.add_field('Be aware:', "\n\n".join(info for info in self.game_infos))
-        embed.set_footer(f"{self.game.player_turn.name}'s turn", icon=self.game.player_turn.d.user.avatar_url)
+            embed.add_field('Be aware:', "\n\n".join(str(info) for info in self.game_infos))
+        embed.set_footer(f"{self.game.player_turn.name}'s turn", icon=self.game.player_turn.user.avatar_url)
+        return embed
     
     def board_to_str(self) -> str:
         string = ""
         b = str(self.game.board)
-        
+        log.debug(b)
         lines = b.split("\n")
         for i, line in enumerate(lines):
             string += f"{self.orientation_number[i]}{line}\n"
         string += f"*️⃣{''.join}"
+        log.debug(string)
         return string
+    
+    @listener(hikari.GuildReactionAddEvent)
+    async def on_reaction_add(self, event: hikari.ReactionAddEvent):
+        log.debug("react")
+        if (
+            not event.message_id == self._message.id
+            or not (emoji := event.emoji_name) in ['1️⃣','2️⃣','3️⃣','4️⃣','5️⃣','6️⃣','7️⃣','8️⃣','9️⃣','🏳']
+            or event.user_id == self.bot.get_me().id
+        ):
+            return
+        message = self._message
+        await message.remove_reaction(emoji, user=event.user_id)
+
+        if emoji in ['1️⃣','2️⃣','3️⃣','4️⃣','5️⃣','6️⃣','7️⃣','8️⃣','9️⃣']:
+            player = self.game.player1 if self.game.player1.user.id == event.user_id else self.game.player2
+            num = 0
+            for i, n in enumerate(self.orientation_number):
+                if n == emoji:
+                    num = i
+            await self.do_turn(num, player) #type: ignore
+            
+            #         elif str(reaction.emoji) == '🏳':
+            #             self.board_description = 'Spiel beendet'
+            #             self.value1_title = f'Zug {self.turn_count}'
+            #             self.board_footer = f'{str(user)[0:-5]} hat aufgegeben'
+            #             await self.create_board()
+            #             await self.reset_variables()
+            #     if reaction.message.id == self.board_message_id and str(user) != "Inu#3395" and str(user) in [str(self.player1),str(self.player2)] and self.gameOver == False: 
+            #         if str(reaction.emoji) == '⏹':
+            #             pass
+            # elif reaction.message.id == self.old_board_message_id and str(user) != "Inu#3395" and self.gameOver == True:
+            #     if str(reaction.emoji) == '🔁':
+            #         await self.four_wins_managemet(self.last_ctx, self.old_player1, self.old_player2)
+            #         await reaction.message.remove_reaction('🔁', user)
+            # elif reaction.message.id == self.board_message_id and str(user) != "Inu#3395" and (str(user) in [str(self.player1),str(self.player2)]) and self.gameOver == False:
+            #     if str(reaction.emoji) == '🏳':
+            #         self.board_description = 'Spiel beendet'
+            #         self.value1_title = f'Zug {self.turn_count}'
+            #         self.board_footer = f'{str(user)[0:-5]} hat aufgegeben'
+            #         await self.create_board()
+            #         await self.reset_variables()
+            
+    async def do_turn(self, column: int, user: HikariPlayer):
+        try:
+            self.game.do_turn(column, user)
+        except WrongPlayerError as e:
+            self.game_infos.append(e)
+        except ColumnNotInFieldError as e:
+            self.game_infos.append(e)
+        except ColumnError as e:
+            self.game_infos.append(e)
+        except IndexError as e:
+            self.game_infos.append(e)
+        except GameOverError as e:
+            self.game_infos.append(e)
+        await self.update_embed()
+        
             
         
     
