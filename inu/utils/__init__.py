993a901a5d9912c00697c42055c83fde3f6c548f
KUR4M4 <52566613+zp33dy@users.noreply.github.com>
2021-12-22 03:06:26 +0100

    added InvokationStats

--- inu/utils/__init__.py
+++ inu/utils/__init__.py
@@ -7,6 +7,7 @@ from .paginators import Paginator
 from .reddit import Reddit
 from .r_channel_manager import DailyContentChannels
 from .grid import Grid
+from .stats import InvokationStats
 
 import logging
 
