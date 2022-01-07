7eda2b1610bafe09cc8e10a3533936f56de5f7be
KUR4M4 <paul.zenker.pz@gmail.com>
2022-01-07 16:06:40 +0100

    added support for timezones

--- inu/ext/commands/reminders.py
+++ inu/ext/commands/reminders.py
@@ -24,13 +24,13 @@ import hikari
 from numpy import isin
 import apscheduler
 from apscheduler.triggers.interval import IntervalTrigger
-from utils.reminders import REMINDER_UPDATE, Reminders
 
-from utils import HikariReminder, Human
+from utils.reminders import REMINDER_UPDATE, Reminders
+from utils import HikariReminder, Human, Table
 
 
 log = logging.getLogger(__name__)
-log.setLevel(logging.INFO)
+log.setLevel(logging.DEBUG)
 
 plugin = lightbulb.Plugin("Basics", "Extends the commands with basic commands", include_datastore=True)
 
@@ -49,21 +49,30 @@ async def create_reminder(ctx: context.Context):
     message_id = 0
     if hasattr(ctx.event, "message_id"):
         message_id = ctx.event.message_id
+    offset_hours = await fetch_hour_offset(ctx.guild_id)
+    log.debug(offset_hours)
     reminder = HikariReminder(
         ctx.channel_id,
         ctx.author.id,
         message_id,
         ctx.options.info,
         ctx,
+        offset_hours=offset_hours,
     )
     await ctx.respond(f"reminding you to: <t:{str(int(reminder.datetime.timestamp()))}>\nor in seconds: `{Human.number(reminder.in_seconds)}`")
 
-@create_reminder.set_error_handler
+#@create_reminder.set_error_handler
 async def on_reminder_error(event: CommandErrorEvent):
     with open("inu/data/text/reminder-help.txt", "r", encoding="utf-8") as f:
         txt = f.read()
     await event.context.respond(f"I am not a fucking trash converter 🖕\n\n{txt}")
     return True
+
+async def fetch_hour_offset(guild_id: int):
+    table = Table("guild_timezones")
+    r = await table.select(["guild_id"], [guild_id])
+    log.debug(r)
+    return r[0]["offset_hours"]
 
 def load(bot: lightbulb.BotApp):
     bot.add_plugin(plugin)
