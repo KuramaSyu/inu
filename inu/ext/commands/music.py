26c1c50788f763cc479d2da73b7eac74582bcf0c
KUR4M4 <52566613+zp33dy@users.noreply.github.com>
2022-01-10 14:37:24 +0100

    Update music.py

--- inu/ext/commands/music.py
+++ inu/ext/commands/music.py
@@ -541,7 +541,7 @@ async def _join(ctx: Context) -> Optional[hikari.Snowflake]:
 
 async def start_lavalink() -> None:
     """Event that triggers when the hikari gateway is ready."""
-    if int(music.bot.lavalink.do_ping):
+    if int(music.bot.data.lavalink.do_ping):
         is_up = False
         retry = 5
         delay = 5
