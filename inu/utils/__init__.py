c797f2ab8f9b35dd73eef4e53c21bd3151f0cf67
KUR4M4 <52566613+zp33dy@users.noreply.github.com>
2022-01-25 19:39:11 +0100

    Update __init__.py

--- inu/utils/__init__.py
+++ inu/utils/__init__.py
@@ -3,7 +3,7 @@ from .models import Singleton
 from .db import Database, Table
 from .string_crumbler import crumble
 from .colors import Colors
-from .paginators import Paginator
+from .paginators import *
 from .reddit import Reddit
 from .r_channel_manager import DailyContentChannels
 from .grid import Grid
