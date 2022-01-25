58d3a621e75a79a263b1c8af14da2148cd6d4b5c
KUR4M4 <52566613+zp33dy@users.noreply.github.com>
2022-01-25 22:36:19 +0100

    small url build fix

--- inu/utils/jikanv4.py
+++ inu/utils/jikanv4.py
@@ -106,7 +106,7 @@ class AioJikanv4:
         return await self._wrap_response(response, url, **kwargs)
 
     def _build_search_url(self, endpoint: str, query: str, **additional) -> str:
-        query = query.replace(" ", "%20").lower()
+        query = query.lower()
         partial_url = dict(q=query)
         partial_url.update(additional)
         query = urlencode(partial_url)
