453d9d1a50e1004871e1c2f81fa382fb73503be3
KUR4M4 <52566613+zp33dy@users.noreply.github.com>
2022-01-25 19:39:50 +0100

    added refresh button for v4 search

--- inu/utils/paginators/anime_pag.py
+++ inu/utils/paginators/anime_pag.py
@@ -2,13 +2,20 @@ from typing import *
 from enum import Enum
 from pprint import pprint
 import random
+import traceback
 
 import hikari
 from hikari import ButtonStyle, ComponentInteraction, Embed
 from hikari.impl import ActionRowBuilder
+import lightbulb
 from numpy import sort
-from common import *
-from common import listener
+from .common import PaginatorReadyEvent
+from .common import Paginator
+from .common import listener
+
+from core import getLogger
+
+log = getLogger(__name__)
 
 
 class SortBy:
@@ -40,14 +47,20 @@ class AnimePaginator(Paginator):
     def __init__(
         self,
         page_s: Union[List[str], List[Embed]],
+        with_refresh_btn: bool = False,
+        old_message: Optional[lightbulb.ResponseProxy] = None,
     ):
-        super().__init__(page_s)
+        self._old_message = old_message
+        self._with_refresh_btn = with_refresh_btn
+        super().__init__(page_s=page_s, timeout=10*8)
 
     def build_default_components(self, position=None) -> Optional[List[Optional[ActionRowBuilder]]]:
         components = super().build_default_components(position)
         if not isinstance(components, list):
             return components
-        components[-1] = components[-1].add_button(ButtonStyle.SECONDARY, "btn_anime_sort").set_label("sort").add_to_container()
+        #components[-1] = components[-1].add_button(ButtonStyle.SECONDARY, "btn_anime_sort").set_label("sort").add_to_container()
+        if self._with_refresh_btn:
+            components[-1] = components[-1].add_button(ButtonStyle.SECONDARY, "btn_anime_re_search").set_label("more information").add_to_container()
         return components
     
     @listener(hikari.InteractionCreateEvent)
@@ -59,6 +72,20 @@ class AnimePaginator(Paginator):
             self._position = 0
             await self.send(self._pages[self._position], interaction=event.interaction)
             return
+        elif event.interaction.custom_id == "btn_anime_re_search":
+            self._stop = True
+            if self._old_message:
+                await self._old_message.delete()
+            await self.bot.rest.delete_messages(self.ctx.channel_id, [self._message.id])
+            try:
+                ext = self.bot.get_plugin("Anime")
+                for cmd in ext.all_commands:
+                    if cmd.name == "anime":
+                        self.ctx._options["name"] = self.pages[self._position].title
+                        await cmd.callback(self.ctx)
+                        return
+            except:
+                log.error(traceback.format_exc())
 
     def _sort_embeds(self, sort_by: SortTypes):
         self._pages = sort_by(self._pages)
