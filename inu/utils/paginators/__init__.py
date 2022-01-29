f3588ab402772a62631cb04f02c2cf9d074c2c55
KUR4M4 <52566613+zp33dy@users.noreply.github.com>
2022-01-29 17:53:04 +0100

    addded MangaPaginator to paginators

--- inu/utils/paginators/__init__.py
+++ inu/utils/paginators/__init__.py
@@ -2,3 +2,4 @@ from .common import Paginator
 from .tag import TagHandler
 from .anime_pag import AnimePaginator
 from .anime_character_pag import AnimeCharacterPaginator
+from .manga_pag import MangaPaginator
