43d253eaf475844740ade15fd86d9d6709f201b1
KUR4M4 <52566613+zp33dy@users.noreply.github.com>
2022-02-16 20:37:15 +0100

    Update __init__.py

--- inu/utils/__init__.py
+++ inu/utils/__init__.py
@@ -4,6 +4,7 @@ from .string_crumbler import crumble, StringCutter, WordIterator
 from .r_channel_manager import DailyContentChannels
 from .grid import Grid
 from .stats import InvokationStats
+from .vote import *
 from .language import Human, Multiple
 from .db_anime import Anime, MyAnimeList
 from .reminders import HikariReminder, Reminders
