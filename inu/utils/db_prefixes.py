d44e70de8ab34db136f220ed793829489d769216
KUR4M4 <52566613+zp33dy@users.noreply.github.com>
2022-02-19 19:37:06 +0100

    Create db_prefixes.py

---
+++ inu/utils/db_prefixes.py
@@ -0,0 +1,56 @@
+from typing import *
+
+from core import getLogger, Table
+
+log = getLogger(__name__)
+
+
+class PrefixManager:
+
+    @classmethod
+    async def add_prefix(
+        cls,
+        guild_id: int,
+        prefix: str,
+    ) -> List[str]:
+        """
+        Returns:
+        --------
+            - (List[str]) prefixes
+        """
+        table = Table("guilds")
+        
+        rec = await table.fetch_by_id("guild_id", guild_id)
+        prefixes = []
+        if rec:
+            prefixes.extend(rec["prefixes"])
+        prefixes.append(prefix)
+        prefixes = list(set(prefixes))
+        await table.upsert(["guild_id", "prefixes"], [guild_id, prefixes])
+        table.db.bot._prefixes[guild_id] = prefixes
+        return prefixes
+
+    @classmethod
+    async def remove_prefix(
+        cls,
+        guild_id: int,
+        prefix: str,
+    ) -> List[str]:
+        """
+        Returns:
+        --------
+            - (List[str]) prefixes
+        """
+        table = Table("guilds")
+        rec = await table.fetch_by_id("guild_id", guild_id)
+        prefixes = []
+        if rec:
+            prefixes.extend(rec["prefixes"])
+        try:
+            prefixes.remove(prefix)
+        except ValueError:
+            pass
+        prefixes = list(set(prefixes))
+        await table.upsert(["guild_id", "prefixes"], [guild_id, prefixes])
+        table.db.bot._prefixes[guild_id] = prefixes
+        return prefixes
