2e2495a04d62f776e8ed77183d57dc05c101654a
paul <paul.zenker.pz@gmail.com>
2022-03-07 16:00:46 +0100

    added PollVoteInstance object

--- inu/utils/paginators/voting.py
+++ inu/utils/paginators/voting.py
@@ -1,12 +1,22 @@
 from . import *
+from ..vote import PollVote
+
 
 
 class ScaleVoteInstance(Paginator):
     def __init__(
-        self,
-        title: str,
-        description: str = "",
+        self, 
+        options: Dict[str, str],
+        active_until: int,
+        anonymous: bool = True,
+        poll_title: str = "Poll",
+        poll_description: str = "",
     ) -> None:
         self.title = title
         self.description = description
         super().__init__()
+
+class PollVoteInstance(Paginator):
+    def __init__(self):
+        self._vote = PollVote(options, active_until, anonymous, poll_title, poll_description)
+        super().__init__([self._vote.embed])
