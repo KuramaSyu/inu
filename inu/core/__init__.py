dd8fdf5d5825d5f9a549643e3b23ef8835fb38f8
KUR4M4 <52566613+zp33dy@users.noreply.github.com>
2022-03-13 21:05:04 +0100

    Update __init__.py

--- inu/core/__init__.py
+++ inu/core/__init__.py
@@ -1,6 +1,6 @@
 from .singleton import Singleton
 from .ping_port import ping
-from .config import ConfigProxy
+from .config import *
 from ._logging import getLogger, LoggingHandler, getLevel
 from .bot import Inu
 from .db import Table, Database
