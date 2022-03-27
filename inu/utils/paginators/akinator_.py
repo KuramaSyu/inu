8777491174bf2b39774cd678f48b4533ad166244
KUR4M4 <52566613+zp33dy@users.noreply.github.com>
2022-03-27 03:10:42 +0200

    finished AkinatorSI

--- inu/utils/paginators/akinator_.py
+++ inu/utils/paginators/akinator_.py
@@ -1,31 +1,135 @@
+from argparse import Action
+from typing import *
 import akinator
 import asyncio
 from akinator.async_aki import Akinator
+from . import Paginator, listener
+import hikari
+import lightbulb
+from lightbulb.context import Context
+from hikari.impl import ActionRowBuilder
+
+from utils import Human
+from core import Inu, getLogger
+
+log = getLogger(__name__)
+
 #from akinator.async_aki import Akinator
 
-aki = Akinator()
 
-async def main():
-    q = await aki.start_game()
+class AkinatorSI(Paginator):
+    def __init__(
+        self,
+        language: str
+    ):
+        super().__init__(page_s="..", timeout=240)
+        self.aki: Akinator = Akinator()
 
-    while aki.progression <= 80:
-        a = input(q + "\n\t")
-        if a == "b":
-            try:
-                q = await aki.back()
-            except akinator.CantGoBackAnyFurther:
-                pass
+    async def start(self, ctx: Context):
+        #await self.post_start()
+        await self.main(ctx)
+
+    async def main(self, ctx: Context):
+        win = False
+        bot: Inu = ctx.bot
+        question = await self.aki.start_game()
+        interaction: Optional[hikari.ComponentInteraction] = None
+        translate_answer={
+            "aki_yes": "yes",
+            "aki_maybe_yes": "probably",
+            "aki_idk": "i don't know",
+            "aki_maybe_no": "probably not",
+            "aki_no": "no",
+            "aki_back": "<-- back",
+            "aki_end": "X exit"
+        }
+        components=[
+            ActionRowBuilder()
+            .add_button(hikari.ButtonStyle.SUCCESS, "aki_yes")
+            .set_label("✔️ Yes")
+            .add_to_container()
+            .add_button(hikari.ButtonStyle.DANGER, "aki_no")
+            .set_label("❌ No")
+            .add_to_container()
+            .add_button(hikari.ButtonStyle.PRIMARY, "aki_idk")
+            .set_label("❔ I don't know")
+            .add_to_container()
+            .add_button(hikari.ButtonStyle.SECONDARY, "aki_maybe_yes")
+            .set_label("Maybe Yes?")
+            .add_to_container()
+            .add_button(hikari.ButtonStyle.SECONDARY, "aki_maybe_no")
+            .set_label("I don't think so")
+            .add_to_container(),
+            ActionRowBuilder()
+            .add_button(hikari.ButtonStyle.PRIMARY, "aki_back")
+            .set_label("◀ Back")
+            .add_to_container()
+            .add_button(hikari.ButtonStyle.PRIMARY, "aki_end")
+            .set_label("You don't get it 🔚")
+            .add_to_container()
+        ]
+        i = 1
+        description = ""
+        while self.aki.progression <= 80:
+            if interaction:
+                asyncio.wait(
+                    [
+                        # await asyncio.create_task(
+                        #     msg.edit(components=[])
+                        # ),
+                        await asyncio.create_task(
+                            interaction.edit_message(
+                                message=(await msg.message()).id,
+                                embed=hikari.Embed(
+                                    title=f"{i}.\n{question}",
+                                    description=Human.short_text(description, 2000),
+                                ),
+                                components=components
+                            )
+                        )
+                    ]
+                )
+            else:
+                msg = await ctx.respond(
+                    embed=hikari.Embed(
+                        title=f"{i}.\n{question}"
+                    ),
+                    components=components,
+                )
+            answer, event, interaction = await bot.wait_for_interaction(
+                custom_ids=["aki_yes", "aki_maybe_yes", "aki_maybe_no", "aki_no", "aki_back", "aki_idk", "aki_end"],
+                user_id=ctx.author.id,
+                channel_id=ctx.channel_id,
+            )
+            await interaction.create_initial_response(
+                hikari.ResponseType.DEFERRED_MESSAGE_UPDATE
+            )
+            description = f"{i}. {question} {translate_answer[answer]}\n{description}"
+            if answer == "aki_back":
+                try:
+                    question = await self.aki.back()
+                except akinator.CantGoBackAnyFurther:
+                    await bot.rest.create_message(ctx.channel_id, "You can't go back any further")
+            elif answer == "aki_end":
+                break
+            else:
+                question = await self.aki.answer(translate_answer[answer])
+            i += 1
+        if win:
+            await self.aki.win()
+
+            await msg.edit(components=[])
+            await ctx.respond(
+                embed=hikari.Embed(
+                    title=f"I think to {self.aki.progression:.0f}% it's {self.aki.first_guess['name']} ({self.aki.first_guess['description']})!"
+                )
+                .set_image(self.aki.first_guess['absolute_picture_path']),
+            )
         else:
-            q = await aki.answer(a)
-    await aki.win()
-
-    correct = input(f"It's {aki.first_guess['name']} ({aki.first_guess['description']})! Was I correct?\n{aki.first_guess['absolute_picture_path']}\n\t")
-    if correct.lower() == "yes" or correct.lower() == "y":
-        print("Yay\n")
-    else:
-        print("Oof\n")
-    await aki.close()
-
-loop = asyncio.get_event_loop()
-loop.run_until_complete(main())
-loop.close()
+            await self.aki.win()
+            await ctx.respond(f"""Well played. These where the last characters I thought of: {', '.join(f"{float(a['proba']):.1f}% {a['name']}" for a in self.aki.guesses)}""")
+        # if correct.lower() == "yes" or correct.lower() == "y":
+        #     print("Yay\n")
+        # else:
+        #     print("Oof\n")
+        await self.aki.close()
