801f964bf16a776310e50cd36823f79d06b81607
KUR4M4 <52566613+zp33dy@users.noreply.github.com>
2022-04-01 23:54:13 +0200

    added BoredIdea Rest API class

---
+++ inu/utils/rest/bored.py
@@ -0,0 +1,38 @@
+from typing import *
+import aiohttp
+import hikari
+{
+  "activity": "Make a couch fort",
+  "type": "recreational",
+  "participants": 1,
+  "price": 0,
+  "link": "",
+  "key": "2352669",
+  "accessibility": 0.08
+}
+class BoredIdea:
+    def __init__(self, response: Dict[str, Union[str, float, int]]):
+        self.response = response
+        self.activity: str = response["activity"]
+        self.type: str = response["type"]
+        self.participants: int = response["participants"]
+        self.price: int = response["price"]
+        self.link: str = response["link"]
+        self.key: str = response["key"]
+        self.accessibility: float = response["accessibility"]
+
+    @property
+    def embed(self) -> hikari.Embed:
+        return hikari.Embed(
+            title=f"{self.activity}",
+            description=f"{self.type} | {self.participants} participants | {self.price}€ | {self.accessibility * 100}% accessibility",
+        ).set_thumbnail()
+
+class BoredAPI:
+    Endpoint = "https://www.boredapi.com/api/activity/"
+    @staticmethod
+    async def fetch_idea(cls) -> Dict[str, str]:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(cls.Endpoint) as resp:
+                json_resp = await resp.json()
+        return BoredIdea(json_resp)
