cb320e131ae54491dde9fe73fc430dcb64fcb6b4
KUR4M4 <52566613+zp33dy@users.noreply.github.com>
2022-04-07 21:52:57 +0200

    Create qalc.py

---
+++ inu/ext/tasks/qalc.py
@@ -0,0 +1,49 @@
+import asyncio
+import typing
+from typing import *
+import random
+import datetime
+import time as tm
+import traceback
+import logging
+from pprint import pformat
+
+import hikari
+from hikari.events.shard_events import ShardReadyEvent
+import lightbulb
+from lightbulb import Plugin
+import apscheduler
+from apscheduler.triggers.interval import IntervalTrigger
+
+from utils import Reddit
+from core.db import Database
+from core import Inu
+from utils import DailyContentChannels
+from utils import Columns as Col
+
+
+from core import getLogger, Bash
+
+log = getLogger(__name__)
+
+plugin = lightbulb.Plugin("Update qalc", "Sends daily automated Reddit pictures")
+
+async def update_qalc_currency():
+    #log.debug("Updating qalc currency")
+    await Bash.execute(["qalc", "-e", "1+1"])
+    #log.debug(f"{s=};{e=}")
+
+@plugin.listener(ShardReadyEvent)
+async def load_tasks(event: ShardReadyEvent):
+    try:
+        log.debug("add scheduler job to update currency")
+        trigger = IntervalTrigger(days=1)
+        bot: Inu = plugin.bot
+        bot.scheduler.add_job(update_qalc_currency, trigger)
+        log.debug("update qalc currency")
+        await update_qalc_currency()
+    except:
+        log.error(traceback.format_exc())
+
+def load(bot: Inu):
+    bot.add_plugin(plugin)
