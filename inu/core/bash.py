8554fbc0d6942fa546dd53fccb8cd1912dc251c1
paul <paul.zenker.pz@gmail.com>
2022-04-07 11:15:11 +0200

    Create bash.py

---
+++ inu/core/bash.py
@@ -0,0 +1,56 @@
+import asyncio
+from typing import *
+
+from ._logging import getLogger
+log = getLogger(__name__)
+
+
+class Bash:
+    @classmethod
+    async def execute(cls, query: List[str]) -> Tuple[str, str]:
+        """
+        Executes a bash command
+        Parameters:
+        query: The bash command to execute
+        Returns:
+        stdout: The stdout of the command
+        stderr: The stderr of the command
+        """
+        proc = await asyncio.create_subprocess_exec(
+            *query,
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.PIPE,
+        )
+
+        stdout, stderr = await proc.communicate()
+        return stdout.decode("utf-8"), stderr.decode("utf-8")
+
+    @classmethod
+    async def qalc(cls, query: str, base: int = 10, terse: bool = True) -> str:
+        """
+        tries to calculate a query
+
+        Parameters:
+        -----------
+        query: 
+            The query to calculate
+
+        Returns:
+        --------
+        str:
+            The result of the calculation
+
+        Raises:
+        -------
+        ValueError: 
+            If the query could not be calculated (error contains stderr)
+        """
+        args = ["qalc"]
+        args.append(f"--base={base}")
+        if terse:
+            args.append("-terse")
+        args.append(query)
+        out, err = await cls.execute(args) # "-t",
+        if err:
+            raise ValueError(err)
+        return out
