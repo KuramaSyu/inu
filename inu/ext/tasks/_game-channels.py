1af6bff84cc48b430e44857193d73df883044264
KUR4M4 <52566613+zp33dy@users.noreply.github.com>
2022-04-23 14:42:58 +0200

    Update _game-channels.py

--- inu/ext/tasks/_game-channels.py
+++ inu/ext/tasks/_game-channels.py
@@ -2,7 +2,7 @@ import asyncio
 import typing
 from typing import *
 import random
-import datetime
+from datetime import datetime
 import time as tm
 import traceback
 import logging
@@ -26,11 +26,28 @@ from core import getLogger
 
 log = getLogger(__name__)
 
-plugin = lightbulb.Plugin("Daily Reddit", "Sends daily automated Reddit pictures", include_datastore=True)
+plugin = lightbulb.Plugin(
+    "Auto Category", 
+    "Creates a category which will be updated with games which are played", 
+    include_datastore=True
+)
 
+# Mapping from guild_id to a mapping from game name to playtime in hours
+games: Dict[int, Dict[str, int]] = {}
 
-async def fetch_current_games(bot: Inu):
-    pass
+async def fetch_current_games(bot: Inu, games: Dict[int, Dict[str, int]]):
+    guild: hikari.Guild
+    now = datetime.now()
+    if now.hour == 0:
+        # TODO: write games to database
+        games = {}
+    for guild in await bot.rest.fetch_my_guilds():
+        for member in await bot.rest.fetch_members(guild.id):
+            if (presence := member.get_presence()):
+                for activity in presence.activities:
+                    log.debug(f"{member.name} is playing {presence.activity.name}")
+                    act_name = activity.name
+                            
 
 @plugin.listener(ShardReadyEvent)
 async def load_tasks(event: ShardReadyEvent):
@@ -42,79 +59,5 @@ async def load_tasks(event: ShardReadyEvent):
     logging.getLogger('apscheduler.executors.default').setLevel(logging.WARNING)
 
 
-async def pics_of_hour():
-    """
-    sends 1x/hour images from a specific subreddit into all channels
-    registered in the database
-    """
-    try:
-        now = datetime.datetime.now()
-        if now.minute != 0:
-            return
-        subreddit = next(plugin.d.subreddits)
-        if not subreddit:
-            return
-        tasks = []
-        for mapping in await DailyContentChannels.get_all_channels(Col.CHANNEL_IDS):
-            log.debug(pformat(mapping))
-            for guild_id, channel_ids in mapping.items():
-                for channel_id in channel_ids:
-                    try:
-                        task = asyncio.create_task(send_top_x_pics(subreddit, channel_id))
-                        tasks.append(task)
-                    except Exception:
-                        await DailyContentChannels.remove_channel(Col.CHANNEL_IDS, channel_id, guild_id)
-                        log.info(f"removed guild channel - was not reachable: {channel_id} from guild: {guild_id}")
-        if not tasks:
-            return
-        _ = await asyncio.gather(*tasks)
-    except Exception:
-        log.critical(traceback.format_exc())
-
-async def send_top_x_pics(subreddit: str, channel_id: int, count: int = 3):
-    hours = int(tm.strftime("%H", tm.localtime()))
-    try:
-        posts = await Reddit.get_posts(
-            subreddit=subreddit,
-            top=True,
-            hot=False,
-            minimum=count,
-            time_filter="day"
-        )
-        # url, title = await get_a_pic(subreddit=str(subreddit), post_to_pick=int(x), hot=False, top=True)
-        if not posts:
-            return
-        for x in range(0, count, 1):
-            embed = hikari.Embed()
-            embed.title = f'{posts[x].title}'
-            embed.set_image(posts[x].url)
-            embed.description = f'[{posts[x].subreddit_name_prefixed}](https://www.reddit.com/{posts[x].subreddit._path})'
-            log.debug(channel_id)
-            await plugin.bot.rest.create_message(channel_id, embed=embed)
-    except Exception as e:
-        log.critical(traceback.format_exc())
-        raise e
-    
-@plugin.listener(hikari.ReactionAddEvent)
-async def on_thumb_up(event: hikari.ReactionAddEvent):
-    if event.emoji_name != "👍":
-        return
-    
-    message = await plugin.bot.rest.fetch_message(event.channel_id, event.message_id)
-    if not message or not message.author.id == plugin.bot.get_me().id:
-        return
-    
-    channel = await plugin.bot.rest.fetch_channel(event.channel_id)
-    top_channels = await DailyContentChannels.get_channels_from_guild(Col.TOP_CHANNEL_IDS, channel.guild_id)
-    log.debug("send to top channel")
-    for ch in top_channels:
-        try:
-            log.debug(ch)
-            await plugin.bot.rest.create_message(ch, embed=message.embeds[0])
-        except Exception:
-            log.error(traceback.format_exc())
-            await plugin.bot.rest.create_message(event.channel_id, "I can't send this message anywhere :/")
-    
-         
 def load(bot: Inu):
     bot.add_plugin(plugin)
