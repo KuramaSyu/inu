cd2616cec72d3d905a927c3864a203a367ae7dea
KUR4M4 <52566613+zp33dy@users.noreply.github.com>
2022-04-28 01:45:59 +0200

    added some methods

--- inu/utils/db/game_categories.py
+++ inu/utils/db/game_categories.py
@@ -51,9 +51,17 @@ class CurrentGamesManager:
     ) -> Optional[Mapping[str, Any]]:
         """inserts guild_id and game into database. Timestamp will be time of method call"""
         table = Table("current_games")
+        now = datetime.now()
+        about_now = datetime(
+            year=now.year,
+            month=now.month,
+            day=now.day,
+            hour=now.hour,
+            minute=now.minute,
+        )
         return await table.insert(
             which_columns=["guild_id", "game", "user_amount", "timestamp"],
-            values=[guild_id, game, amount, datetime.now()],
+            values=[guild_id, game, amount, about_now],
         )
 
     @classmethod
@@ -67,7 +75,11 @@ class CurrentGamesManager:
         return await table.fetch(sql, when_older_than)
 
     @classmethod
-    async def fetch_games(cls, guild_id: int, since: datetime) ->Optional[List[Mapping[str, Any]]]:
+    async def fetch_games(
+        cls, 
+        guild_id: int, 
+        since: datetime
+    ) ->Optional[List[Mapping[str, Any]]]:
         """
         Args:
             guild_id (int): /
@@ -77,10 +89,53 @@ class CurrentGamesManager:
         """
         table = Table("current_games")
         sql = (
-            f"SELECT game, SUM(user_amount) AS amount\n"
+            f"SELECT game, SUM(user_amount) AS amount, MIN(timestamp) AS first_occurrence\n"
             f"FROM {table.name}\n"
             f"WHERE guild_id = $1 AND timestamp > $2\n"
             f"GROUP BY game"
         )
         return await table.fetch(sql, guild_id, since)
 
+    @classmethod
+    async def fetch_raw_activities(
+        cls, 
+        guild_id: int, 
+        since: datetime
+    ) -> Optional[List[Mapping[str, Any]]]:
+        """
+        Args:
+            guild_id (int): /
+
+        Returns:
+            Dict[str, int]: Mapping from game name to the time in minutes/10 played it
+        """
+        table = Table("current_games")
+        sql = (
+            f"SELECT game, user_amount, timestamp\n"
+            f"FROM {table.name}\n"
+            f"WHERE guild_id = $1 AND timestamp > $2"
+        )
+        return await table.fetch(sql, guild_id, since)
+
+    @classmethod
+    async def fetch_total_activity_by_timestamp(
+        cls, 
+        guild_id: int, 
+        since: datetime,
+    ) -> List[Mapping[str, Any]]:
+        """
+        Args:
+            guild_id (int): /
+
+        Returns:
+            Dict[datetime, int]: Mapping from timestamp to amount of users playing the game
+        """
+        table = Table("current_games")
+        sql = (
+            f"SELECT ts_round(timestamp, 300) AS round_timestamp, SUM(user_amount) AS total_user_amount\n"
+            f"FROM {table.name}\n"
+            f"WHERE guild_id = $1 AND timestamp > $2\n"
+            f"GROUP BY round_timestamp\n"
+            f"ORDER BY round_timestamp"
+        )
+        return await table.fetch(sql, guild_id, since)
