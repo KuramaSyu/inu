e4bb8bdf02e2a2678f8dffff8a1215c10e17acfc
paul <paul.zenker.pz@gmail.com>
2022-06-08 16:09:43 +0200

    added reactionaddlistener

--- inu/ext/commands/_polls.py
+++ inu/ext/commands/_polls.py
@@ -22,15 +22,27 @@ from matplotlib.style import available
 from numpy import full, isin
 from fuzzywuzzy import fuzz
 
-from utils import Colors, Human, Paginator, crumble, PollVote
+from utils import Colors, Human, Paginator, crumble, Poll, PollManager
 from core import getLogger, Inu
 
 log = getLogger(__name__)
 
 plugin = lightbulb.Plugin("Polls")
 
-active_polls: List[PollVote] = []
+active_polls: List[Poll] = []
+letter_emojis = [f':regional_indicator_{l}:' for l in 'abcdefghijklmnop']
 # at start init from database
+
+
+@plugin.listener(hikari.GuildReactionAddEvent)
+async def on_reaction_add(event: hikari.ReactionAddEvent):
+    if event.emoji_name not in letter_emojis:
+        return
+    if not (poll := PollManager.get_poll(event.message_id, event.channel_id)):
+        return
+    await poll.add_vote_and_update(event.user_id, 1, event.emoji_name)
+    
+
 
 @plugin.listener(lightbulb.events.LightbulbStartedEvent)
 async def reinit_open_polls():
@@ -40,4 +52,19 @@ async def reinit_open_polls():
 @lightbulb.command("poll", "start a poll")
 @lightbulb.implements(commands.PrefixCommand, commands.SlashCommand)
 async def make_poll(ctx: Context):
-    pass
+    bot: Inu = plugin.bot
+    responses, interaction, _ = await bot.shortcuts.ask_with_modal(
+        modal_title="Creating a poll", 
+        question_s=[
+            "Poll Headline:", "Poll Description:", "Poll Options:", 
+            "Poll Duration:", "Anonymous?"
+        ], 
+        placeholder_s=[
+            "How often do you smoke weed?", 
+            "... additional information here if needed ...", 
+            "Yes, every day, sometimes, one time, never had and never will be",
+            "2 days 5 hours",
+            "yes (yes|no|maybe)"
+        ],
+        interaction=ctx.interaction
+    )
