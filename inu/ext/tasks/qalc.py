03aa4e9eb6e9e5d8da3bed173df097397e568cce
KUR4M4 <paul.zenker.pz@gmail.com>
2022-07-27 23:36:52 +0200

    added job for updating qalc currency

--- inu/ext/tasks/qalc.py
+++ inu/ext/tasks/qalc.py
@@ -25,25 +25,30 @@ from utils import Columns as Col
 from core import getLogger, Bash
 
 log = getLogger(__name__)
+loaded = False
 
 plugin = lightbulb.Plugin("Update qalc", "Sends daily automated Reddit pictures")
 
 async def update_qalc_currency():
-    #log.debug("Updating qalc currency")
-    await Bash.execute(["qalc", "-e", "1+1"])
-    #log.debug(f"{s=};{e=}")
+    # -e = updating currency
+    result = await Bash.execute(["qalc", "-e", "x EUR = 1 BTC"])
+    log.debug(f"Updated qalc currency")
 
-# @plugin.listener(ShardReadyEvent)
-# async def load_tasks(event: ShardReadyEvent):
-#     try:
-#         log.debug("add scheduler job to update currency")
-#         trigger = IntervalTrigger(days=1)
-#         bot: Inu = plugin.bot
-#         bot.scheduler.add_job(update_qalc_currency, trigger)
-#         log.debug("update qalc currency")
-#         await update_qalc_currency()
-#     except:
-#         log.error(traceback.format_exc())
+@plugin.listener(ShardReadyEvent)
+async def load_tasks(event: ShardReadyEvent):
+    global loaded
+    if loaded:
+        return
+    loaded = True
+    try:
+        log.info("scheduled job for updating currency")
+        trigger = IntervalTrigger(hours=3)
+        bot: Inu = plugin.bot
+        bot.scheduler.add_job(update_qalc_currency, trigger)
+        
+        await update_qalc_currency()
+    except:
+        log.error(traceback.format_exc())
 
 def load(bot: Inu):
     bot.add_plugin(plugin)
