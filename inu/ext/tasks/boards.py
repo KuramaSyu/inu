fd63c132e4505f2ca9a0f8d56d00f99e33335c0d
paul <paul.zenker.pz@gmail.com>
2022-08-26 13:57:29 +0200

    fully implemented on_reaction_add

--- inu/ext/tasks/boards.py
+++ inu/ext/tasks/boards.py
@@ -5,12 +5,7 @@ Handling that the boards are actually working:
     - remove boards when the bot leaves a guild
 """
 
-import typing
-from typing import (
-    Union,
-    Optional,
-    List,
-)
+from typing import *
 import asyncio
 import logging
 from datetime import datetime, timedelta
@@ -24,15 +19,17 @@ from hikari import Embed
 import apscheduler
 from apscheduler.triggers.interval import IntervalTrigger
 from utils.db import BoardManager
+import asyncpg
 
 from core import Table, getLogger, Inu
+from utils import make_message_link, Colors
 
 log = getLogger(__name__)
 METHOD_SYNC_TIME: int
 SYNCING = False
 bot: Inu
 
-plugin = lightbulb.Plugin("poll loader", "loads polls from database")
+plugin = lightbulb.Plugin("board tracker", "tracks messages if they where pinned")
 
 @plugin.listener(hikari.ShardReadyEvent)
 async def load_tasks(event: hikari.ShardReadyEvent):
@@ -55,13 +52,6 @@ async def init_method():
 async def method():
     pass
 
-async def update_message(
-    message_id: int,
-    emoji: str,
-):
-    # fetch information from db and recreate or delete message
-    ...
-
 @plugin.listener(hikari.GuildReactionAddEvent)
 async def on_reaction_add(event: hikari.GuildReactionAddEvent):
     log.debug(f"receiving: {event.emoji_name}")
@@ -69,23 +59,62 @@ async def on_reaction_add(event: hikari.GuildReactionAddEvent):
 
     # guild has no board with this reaction
     if not BoardManager.has_emoji(event.guild_id, event.emoji_name):
+        log.debug(f"emoji not tracked")
         return
 
     # board don't has this message -> create entry
-    if not BoardManager.has_message_id(event.guild_id, event.emoji_name, event.message_id):
-        await BoardManager.add_entry(
-            message_id=event.message_id,
+    # if not BoardManager.has_message_id(event.guild_id, event.emoji_name, event.message_id):
+    #     log.debug("board don't has this message -> create entry")
+    #     await BoardManager.add_entry(
+    #         message_id=event.message_id,
+    #         guild_id=event.guild_id,
+    #         emoji=event.emoji_name,
+    #     )
+    
+    message: Optional[hikari.Message] = None
+    entry = await BoardManager.fetch_entry(event.message_id, event.emoji_name)
+    if not entry:
+        log.debug(f"no entry found => add entry")
+        message = await bot.rest.fetch_message(event.channel_id, event.message_id)
+        if message is None:
+            return
+        content = f"{message.content}"
+        # add first embed to content
+        if len(message.embeds) > 0:
+            if message.embeds[0].title:
+                content += f"\n\n**{message.embeds[0].title}**"
+            if message.embeds[0].description:
+                content += f"\n{message.embeds[0].description}"
+        if not message:
+            log.debug("message not found")
+            return
+        # add entry
+        entry = (await BoardManager.add_entry(
             guild_id=event.guild_id,
+            message_id=event.message_id,
+            author_id=message.author.id,
+            channel_id=message.channel_id,
+            emoji=event.emoji_name,
+            content=content,
+            attachment_urls=[a.url for a in message.attachments]
+        ))[0]
+
+    # add a reaction
+    log.debug(f"{entry=}")
+    log.debug("add a reaction to db")
+    try:
+        await BoardManager.add_reaction(
+            guild_id=event.guild_id,
+            message_id=event.message_id,
+            reacter_id=event.user_id,
             emoji=event.emoji_name,
         )
-    
-    # add a reaction
-    await BoardManager.add_reaction(
-        guild_id=event.guild_id,
-        message_id=event.message_id,
-        reacter_id=event.user_id,
-        emoji=event.emoji_name,
-    )
+    except asyncpg.UniqueViolationError:
+        log.debug(f"Violation error - reaction was already added")
+    await update_message(entry, message)
+
+
+
 
     # method for updating a message in a board
 
@@ -108,7 +137,7 @@ async def on_reaction_remove(event: hikari.GuildReactionDeleteEvent):
             emoji=event.emoji_name,
         )
     except Exception:
-        log.waring(f"insertion error, which shouldn't occure.\n{traceback.format_exc()}")
+        log.warning(f"insertion error, which shouldn't occure.\n{traceback.format_exc()}")
     # TODO: when a message has no reactions any more, delete it 
     # (db will be cleared automatically -> when updating message results in error, delete message)
 
@@ -123,28 +152,79 @@ async def on_guild_leave(event: hikari.GuildLeaveEvent):
     ...
 
 async def update_message(
-    message_id: int,
-    channel_id: int,
-    emoji: str,
-    update_message: bool,
-    **kwargs,
-    
+    board_entry: Dict[str, Any],
+    message: Optional[hikari.Message] = None,
 ):
-    message_votes = await BoardManager.fetch_entries(message_id)
-    entry = await BoardManager
-    if update_message:
-        embed = Embed()
-        embed.set_author()
-        text = f"{len(message_votes)}x {emoji}"
+    channel_id = board_entry["channel_id"]
+    message_id = board_entry["message_id"]
+    guild_id = board_entry["guild_id"]
+    content = board_entry["content"]
+    emoji = board_entry["emoji"]
+
+    author = await bot.mrest.fetch_member(guild_id, board_entry["author_id"])
+    if not author:
+        log.warning(f"no member with id {board_entry['author_id']} found")
+        return
+
+    message_votes = await BoardManager.fetch_reactions(message_id)
+    board = await BoardManager.fetch_board(guild_id, emoji)
+
+    color_stages = {
+        n: color for n, color in zip(
+            range(1,100),
+            [
+                "slateblue", "mediumslateblue",
+                "mediumpurple", "blueviolet", "indigo",
+                "darkorchid", "darkviolet", "mediumorchid",
+                "purple", "darkmagenta", "mediumvioletred",
+                "deeppink", "crimson"
+            ]
+        )
+    }
+    color = color_stages.get(len(message_votes), "crimson")
+    reaction_content = f"{len(message_votes)}x {emoji}"
+    embed = Embed()
+    embed.set_author(
+        name=f"{author.username}", 
+        icon=author.display_avatar_url
+    )
+    embed.description = (
+        f"[Jump to the message in <#{board['channel_id']}>]"
+        f"({make_message_link(guild_id, channel_id, message_id)})\n\n{content}"
+    )
+    # embed color -> how many stars
+    embed.color = Colors.from_name(color)
+
+    if not board_entry["board_message_id"]:
+        if not message:
+            raise RuntimeError(
+                "in update_message:\nif board entry gets created first time, a message musst be passed in"
+            )
+        kwargs = {
+            "attachments": board_entry['attachment_urls'],
+            "embed": embed,
+            "content": reaction_content,
+            "components": message.components,
+            "channel": board['channel_id'],
+        }
+
+        board_message = await bot.rest.create_message(**kwargs)
+        # set board_message_id
+        await BoardManager.edit_entry(
+            message_id, 
+            emoji, 
+            None, 
+            board_message_id=board_message.id,
+        )
     else:
-        message: hikari.Message = await bot.rest.fetch_message(
-            channel=channel_id,
-            message=message_id,
+        await bot.rest.edit_message(
+            board['channel_id'], 
+            board_entry['board_message_id'], 
+            embed=embed,
+            content=reaction_content,
+            attachments=board_entry['attachment_urls']
         )
-        embed = Embed(
-            description=f"{message.content}"
-        )
-                
+
     
 
 def load(inu: Inu):
