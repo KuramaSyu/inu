f9dbda7a1b26d6b3ea7dab65ea6e062daae6f97e
paul <paul.zenker.pz@gmail.com>
2022-08-30 16:05:28 +0200

    added stopwatch

--- inu/core/__init__.py
+++ inu/core/__init__.py
@@ -1,7 +1,7 @@
 from .singleton import Singleton
 from .ping_port import ping
 from .config import *
-from ._logging import getLogger, LoggingHandler, getLevel
+from ._logging import getLogger, LoggingHandler, getLevel, stopwatch
 from .bash import Bash
 from .bot import Inu, BotResponseError # needs `Bash`
 from .db import Table, Database  # needs `Inu`
