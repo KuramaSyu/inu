943b29608dc74e4ea16e0a16f72d4313012831c1
paul <paul.zenker.pz@gmail.com>
2022-11-23 10:30:46 +0100

    added some doc strings

--- inu/utils/db/facts.py
+++ inu/utils/db/facts.py
@@ -12,15 +12,17 @@ class Facts:
 
     @classmethod
     async def add_fact(cls, fact: str) -> bool:
+        """add fact to db"""
         hs = hashlib.sha256(fact.encode("utf-8")).hexdigest() 
         try:
-            await cls._table.insert(["type", "fact", "sha256"], ["basic", fact, hs])
+            await cls._table.insert(["type", "fact", "sha256"], ["basic", fact, hs], on_conflict="DO NOTHING")
         except Exception:
             return False
         return True
 
     @classmethod
     async def fetch_random_fact(cls) -> str | None:
+        """fetch random fact from db"""
         try:
             records = await cls._table.fetch(f"SELECT * FROM {cls._table.name} ORDER BY random() limit 1;")
             return records[0]["fact"]
@@ -29,6 +31,7 @@ class Facts:
 
     @classmethod
     async def _cache_resp(cls, resp: List[Dict[str, str]]) -> None:
+        """cache RSET resp to db"""
         tasks = [asyncio.create_task(Facts.add_fact(entry["fact"])) for entry in resp]
         done, pending = await asyncio.wait(tasks, return_when=asyncio.ALL_COMPLETED)
         sum_ = 0
@@ -40,6 +43,13 @@ class Facts:
 
     @classmethod
     async def _fetch_from_rest(cls, amount: int = 30) -> int:
+        """fetch REST resp and cache it. 
+        
+        Args:
+        -----
+        amount : int
+            the amount of facts from 0 - 30
+        """
         resp = await RESTFacts.fetch_facts(amount=amount)
         asyncio.create_task(cls._cache_resp(resp))
         return resp
