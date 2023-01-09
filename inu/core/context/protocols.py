635e6a05fa1da2e27074835e3e7d1d84d0bf040b
KUR4M4 <52566613+zp33dy@users.noreply.github.com>
2023-01-09 16:03:01 +0100

    added ABC

--- inu/core/context/protocols.py
+++ inu/core/context/protocols.py
@@ -1,11 +1,36 @@
 from typing import *
+from abc import ABC, abstractmethod
 
+import hikari
 from lightbulb.context import Context
 
 
 T = TypeVar("T")
 
 
+class InuContext(ABC):
+    @abstractmethod
+    def from_context(cls: Context, ctx: Context) -> T:
+        ...
+
+    @abstractmethod
+    def from_event(cls: Context, event: hikari.Event) -> T:
+        ...
+    
+    @property
+    @abstractmethod
+    def original_message(self) -> hikari.Message:
+        ...
+
+
+
 class InuContextProtocol(Protocol[T]):
     def from_context(cls: Context, ctx: Context) -> T:
+        ...
+    
+    def from_event(cls: Context, event: hikari.Event) -> T:
+        ...
+
+    @property
+    def original_message(self) -> hikari.Message:
         ...
