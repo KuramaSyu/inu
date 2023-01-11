76c1b3d99909e9b38cd527adb077c8a31ae016b0
KUR4M4 <52566613+zp33dy@users.noreply.github.com>
2023-01-11 11:47:39 +0100

    added get_guild_or_channel_id(interaction) method

--- inu/utils/shortcuts.py
+++ inu/utils/shortcuts.py
@@ -1,7 +1,17 @@
 from typing import *
+
+import hikari
+
+
 def make_message_link(
     guild_id: int,
     channel_id: int,
     message_id: int,
 ):
     return f"https://discord.com/channels/{guild_id}/{channel_id}/{message_id}"
+
+
+def get_guild_or_channel_id(interaction: hikari.ComponentInteraction) -> int:
+    if isinstance(interaction.user, hikari.Member):
+        return interaction.user.guild_id
+    return interaction.channel_id
