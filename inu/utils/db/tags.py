7dbd9dcf350ebb7a3291b7719184d3e84b9d6b39
KUR4M4 <52566613+zp33dy@users.noreply.github.com>
2023-01-14 18:25:59 +0100

    added 2 methods for checking permissions

--- inu/utils/db/tags.py
+++ inu/utils/db/tags.py
@@ -262,8 +262,11 @@ class Tag():
         new_tag.is_local_available = not local_taken
         return new_tag
 
-    async def is_authorized_to_see(self, user: hikari.Member | hikari.User) -> bool:
-        return
+    async def is_authorized_to_see(self, guild_or_channel_id: int) -> bool:
+        return guild_or_channel_id in self.guild_ids
+
+    async def is_authorized_to_write(self, user_id: int) -> bool:
+        return user_id in self.owners
 
     @classmethod
     async def from_id(cls, tag_id: int, user_id: int, guild_or_channel_id: int = 0) -> Optional[Mapping[str, Any]]:
