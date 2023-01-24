e381957d34512e0cac61187a17f2043c512c2605
KUR4M4 <52566613+zp33dy@users.noreply.github.com>
2023-01-24 16:58:39 +0100

    Update __init__.py

--- inu/utils/__init__.py
+++ inu/utils/__init__.py
@@ -1,4 +1,5 @@
 
+from .shortcuts import *
 from .colors import Colors
 from .string_crumbler import crumble, StringCutter, WordIterator
 from .language import Human, Multiple, get_date_format_by_timedelta
@@ -9,7 +10,6 @@ from .rest import *
 from .db import *
 from .logger import *
 from .poll import Poll
-from .shortcuts import *
 
 from .emojis import Emoji
 
