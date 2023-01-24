a3f16bb15996d69d61c3e2be56688518a52721b8
KUR4M4 <52566613+zp33dy@users.noreply.github.com>
2023-01-24 16:59:12 +0100

    added method for returning guild name or id

--- inu/utils/shortcuts.py
+++ inu/utils/shortcuts.py
@@ -12,6 +12,24 @@ def make_message_link(
 
 
 def get_guild_or_channel_id(interaction: hikari.ComponentInteraction) -> int:
+    """
+    Returns the guild_id if not None, otherwise the (DM) channel_id 
+    """
     if isinstance(interaction.user, hikari.Member):
         return interaction.user.guild_id
     return interaction.channel_id
+
+def guild_name_or_id(guild_id: int, bot: hikari.CacheAware) -> str:
+    """
+    returns the name of the guild_id if in cache, otherwise the ID as string
+
+    Args:
+    -----
+    guild_id : int
+        the id of the guild
+    bot : hikari.CacheAware
+        A cache aware bot, to check if guild is in cache
+    """
+    guild = bot.cache.get_guild(guild_id)
+    return guild.name if guild else guild_id
+
