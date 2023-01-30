0ae606e918939dafed8f58cd00ddb7786cda615e
KUR4M4 <52566613+zp33dy@users.noreply.github.com>
2023-01-30 14:39:02 +0100

    added respond method

--- inu/core/context/protocols.py
+++ inu/core/context/protocols.py
@@ -32,6 +32,35 @@ class InuContext(ABC):
     def user(self) -> hikari.User:
         ...
 
+    @abstractmethod
+    async def respond(self, *args, **kwargs):
+        """
+        Create a response for this context. The first time this method is called, the initial
+        interaction response will be created by calling
+        :obj:`~hikari.interactions.command_interactions.CommandInteraction.create_initial_response` with the response
+        type set to :obj:`~hikari.interactions.base_interactions.ResponseType.MESSAGE_CREATE` if not otherwise
+        specified.
+
+        Subsequent calls will instead create followup responses to the interaction by calling
+        :obj:`~hikari.interactions.command_interactions.CommandInteraction.execute`.
+
+        Args:
+            update : bool
+                wether or not to update the current interaction message
+            *args (Any): Positional arguments passed to ``CommandInteraction.create_initial_response`` or
+                ``CommandInteraction.execute``.
+            delete_after (Union[:obj:`int`, :obj:`float`, ``None``]): The number of seconds to wait before deleting this response.
+            **kwargs: Keyword arguments passed to ``CommandInteraction.create_initial_response`` or
+                ``CommandInteraction.execute``.
+
+        Returns:
+            :obj:`~ResponseProxy`: Proxy wrapping the response of the ``respond`` call.
+
+        .. versionadded:: 2.2.0
+            ``delete_after`` kwarg.
+        """
+        ...
+
 
 
 class InuContextProtocol(Protocol[T]):
