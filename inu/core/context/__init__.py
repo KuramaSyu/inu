d40a7d7f87440ac8d6797295c4b05ae79b046e73
KUR4M4 <52566613+zp33dy@users.noreply.github.com>
2023-01-31 13:18:02 +0100

    added InteractionContext for ComponentInteractions

--- inu/core/context/__init__.py
+++ inu/core/context/__init__.py
@@ -1,4 +1,4 @@
 from .protocols import InuContext, InuContextProtocol
-from .interactions import InteractionContext
+from .interactions import InteractionContext, CommandInteractionContext
 from .rest import RESTContext
 from .factory import get_context
