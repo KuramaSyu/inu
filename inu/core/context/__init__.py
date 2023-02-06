67f7f816027349571a7aadb3e20aceaf44a22419
KUR4M4 <52566613+zp33dy@users.noreply.github.com>
2023-02-06 15:51:01 +0100

    Update __init__.py

--- inu/core/context/__init__.py
+++ inu/core/context/__init__.py
@@ -1,4 +1,5 @@
 from .protocols import InuContext, InuContextProtocol
+from .base import InuContextBase, UniqueContextInstance
 from .interactions import InteractionContext, CommandInteractionContext
 from .rest import RESTContext
 from .factory import get_context
