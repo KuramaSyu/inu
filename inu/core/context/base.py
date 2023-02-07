93ecfacf1b47cb96bb11a26f21459663515fe6d6
KUR4M4 <52566613+zp33dy@users.noreply.github.com>
2023-02-07 15:03:46 +0100

    added TTL cache to `_get`

--- inu/core/context/base.py
+++ inu/core/context/base.py
@@ -2,6 +2,7 @@ from weakref import WeakValueDictionary
 from abc import abstractmethod
 from typing import *
 
+from cachetools import cached, TTLCache
 from core import getLogger
 from . import InuContext
 
@@ -14,12 +15,20 @@ class UniqueContextInstance:
 
     @classmethod
     def get(cls, self: InuContextT) -> InuContextT:
-        if (instance := cls._instances.get(self.id)) is not None:
-            log.debug(f"return existing instance with type {type(self)} and ID {self.id}")
-            return instance  # type: ignore
-        if self.id:
-            log.debug(f"create instance with type {type(self)} ID {self.id}")
-            cls._instances[self.id] = self
+        if not self.is_hashable:
+            return self
+        return cls._get(self)
+
+    @classmethod
+    @cached(cache=TTLCache(maxsize=1024, ttl=60))
+    def _get(cls, self: InuContextT) -> InuContextT:
+        # if (instance := cls._instances.get(self.id)) is not None:
+        #     print(f"return existing instance with type {type(self)} and ID {self.id}")
+        #     return instance  # type: ignore
+        # #if self.id:
+        # print(f"create instance with type {type(self)} ID {self.id}")
+        # cls._instances[self.id] = self
+        # print(f"return instance with type {type(self)} ID {self.id}")
         return self
 
 
@@ -39,4 +48,9 @@ class ContextEqualTrait:
 
 
 class InuContextBase(ContextEqualTrait):
-    ...
+    def __hash__(self) -> int:
+        return self.id
+
+    @property
+    def is_hashable(self) -> bool:
+        return self.id is not None
