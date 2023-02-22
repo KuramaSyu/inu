687c3004bd93512af41d50fc8699f3cf8a4b0bea
KUR4M4 <52566613+zp33dy@users.noreply.github.com>
2023-02-22 15:03:04 +0100

    fixed typos

--- inu/ext/commands/errors.py
+++ inu/ext/commands/errors.py
@@ -22,18 +22,19 @@ bot: Inu
 
 
 ERROR_JOKES = [
-    "Wait, there was a difference between beta and production?",
+    "Wait, there is a difference between beta and production?",
     (
-        '_"NOOoo the bot wasn\'t tested properly" is what you are maybe thinking right now. '
-        "So now is the right moment to say you: _you are the tester_"
+        '_"NOOoo the bot wasn\'t tested properly"_ is what you are maybe thinking right now. '
+        "So now is the right moment to tell you: _you **are** the tester_"
     ),
     "Seems like someone was to lazy to test me -- _again_",
     "Y'know: _my_ ordinary life is generating errors",
     "You expected something else? Oh please. I thought you got it by now",
     (
-        "Y'know I can smell you disappointment. It's right about here: ```\n"
-        "|--------------------|\n"
-        "                           ^\n```"
+        "Y'know I can smell your disappointment. It's right about here: ```\n"
+        "good                  bad"
+        "  |--------------------|\n"
+        "                             ^\n```"
     )
 
 ]
