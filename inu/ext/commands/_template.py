73cb27fbb0aa8f90fd84ac1dbba50eba57fc8db4
KUR4M4 <52566613+zp33dy@users.noreply.github.com>
2023-02-24 09:51:03 +0100

    cleaned up template imports

--- inu/ext/commands/_template.py
+++ inu/ext/commands/_template.py
@@ -1,48 +1,32 @@
 import asyncio
-import logging
-import typing
-from datetime import datetime
 from typing import *
-from numpy import full, isin
-
-import aiohttp
+from datetime import datetime
 import hikari
 import lightbulb
-import lightbulb.utils as lightbulb_utils
 
 from fuzzywuzzy import fuzz
 from hikari import (
-    ActionRowComponent, 
-    Embed, 
-    MessageCreateEvent, 
-    embeds, 
+    Embed,
     ResponseType, 
-    TextInputStyle
+    TextInputStyle,
 )
-from hikari.events import InteractionCreateEvent
-from hikari.impl.special_endpoints import ActionRowBuilder, LinkButtonBuilder
-from hikari import ButtonStyle
-from jikanpy import AioJikan
-from lightbulb import OptionModifier as OM
+from hikari.impl import MessageActionRowBuilder
 from lightbulb import commands, context
-from lightbulb.context import Context
-from matplotlib.style import available
-from typing_extensions import Self
 
 
 from utils import (
     Colors, 
     Human, 
     Paginator, 
-    Reddit, 
-    Urban, 
     crumble
 )
 from core import (
     BotResponseError, 
     Inu, 
     Table, 
-    getLogger
+    getLogger,
+    InuContext,
+    get_context
 )
 
 log = getLogger(__name__)
