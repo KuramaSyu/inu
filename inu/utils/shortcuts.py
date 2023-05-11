a411e84a5d9e9668826901c462b7ac199f6663bb
KUR4M4 <52566613+zp33dy@users.noreply.github.com>
2023-05-11 13:29:22 +0200

    added internet resource

--- inu/utils/shortcuts.py
+++ inu/utils/shortcuts.py
@@ -5,6 +5,9 @@ import aiohttp
 
 from core import Inu
 
+# Pictures
+MAGIC_ERROR_MONSTER = "https://media.discordapp.net/attachments/818871393369718824/1106177322069012542/error-monster-1.png?width=1308&height=946"
+
 bot: Inu
 
 def set_bot(bot_: Inu):
