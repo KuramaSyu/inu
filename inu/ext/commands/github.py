59d22c2b64533388db82e2e3cef6d5d81a0fdb8a
KUR4M4 <52566613+zp33dy@users.noreply.github.com>
2023-05-13 02:26:50 +0200

    ADDED COMMAND: change-log
    A command which shows last made changes to this bot.

---
+++ inu/ext/commands/github.py
@@ -0,0 +1,76 @@
+import asyncio
+from typing import *
+from datetime import datetime
+import hikari
+import lightbulb
+
+from fuzzywuzzy import fuzz
+from hikari import (
+    Embed,
+    ResponseType, 
+    TextInputStyle,
+)
+from hikari.impl import MessageActionRowBuilder
+from lightbulb import commands, context
+
+
+from utils import (
+    Colors, 
+    Human, 
+    Paginator, 
+    crumble,
+    GitHubAPI,
+    Commit,
+)
+from core import (
+    BotResponseError, 
+    Inu, 
+    Table, 
+    getLogger,
+    InuContext,
+    get_context
+)
+
+log = getLogger(__name__)
+
+plugin = lightbulb.Plugin("Name", "Description")
+bot: Inu
+
+@plugin.command
+@lightbulb.command("change-log", "Last changes of the bot")
+@lightbulb.implements(commands.SlashCommand, commands.PrefixCommand)
+async def change_log(_ctx: context.Context):
+    ctx = get_context(_ctx.event)
+    github = GitHubAPI.INU_REPO()
+    commits = await github.fetch_commits()
+    embeds: List[Embed] = [
+        Embed("Last Changes", color=Colors.pastel_color())
+    ]
+    for commit in commits:
+        if not commit.has_keywords(commit.DEFAULT_KEYWORDS):
+            continue
+        if len(embeds[-1].fields) >= 24:
+            embeds.append(Embed())
+        value = f"{commit.date_string}{'-'*20}\n\n{commit.description}"
+        embeds[-1].add_field(
+            name=commit.message,
+            value=value,
+        )
+    embeds = [embed for embed in embeds if embed.fields]
+    if not embeds:
+        return await ctx.respond("No changes found", ephemeral=True)
+    paginator = Paginator(page_s=embeds)
+    await paginator.start(ctx)
+    
+
+        
+
+
+
+
+
+def load(inu: lightbulb.BotApp):
+    global bot
+    bot = inu
+    inu.add_plugin(plugin)
+
