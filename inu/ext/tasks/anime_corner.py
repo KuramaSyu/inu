d7fabed602614f05b0f25027ab0a82d4cacc735a
KUR4M4 <52566613+zp33dy@users.noreply.github.com>
2023-07-12 20:16:29 +0200

    Create anime_corner.py

---
+++ inu/ext/tasks/anime_corner.py
@@ -0,0 +1,73 @@
+
+from typing import *
+import asyncio
+import logging
+from datetime import datetime, timedelta, time, date
+
+import lightbulb
+from lightbulb.commands.base import OptionModifier as OM
+import hikari
+import apscheduler
+from apscheduler.triggers.interval import IntervalTrigger
+from humanize import naturaldelta
+from core import Table, getLogger, Inu, stopwatch
+from utils import Reddit, AnimeCornerAPI, AnimeCornerPaginator2
+
+log = getLogger(__name__)
+METHOD_SYNC_TIME: 60*60*24
+SYNCING = False
+TARGET_TIME = time(16,00)
+TRIGGER_NAME = "Anime Corner Trigger"
+bot: Inu
+
+plugin = lightbulb.Plugin("poll loader", "loads polls from database")
+
+@plugin.listener(hikari.ShardReadyEvent)
+async def load_tasks(event: hikari.ShardReadyEvent):
+    global SYNCING
+    if SYNCING:
+        return
+    else:
+        SYNCING = True
+    await asyncio.sleep(3)
+    await method()
+    logging.getLogger('apscheduler.executors.default').setLevel(logging.WARNING)
+    await init_method()
+    await defer_trigger_to_time()
+
+
+async def defer_trigger_to_time(target_time: time | None = TARGET_TIME):
+    if target_time is not None:
+        current_time = datetime.now().time()
+        target_datetime = datetime.combine(date.today(), target_time)
+
+        if target_datetime.time() < current_time:
+            target_datetime += timedelta(days=1)
+
+        wait_time = (target_datetime - datetime.now()).total_seconds()
+        log.info(f"Waiting for {naturaldelta(timedelta(seconds=wait_time))} to shedule the {TRIGGER_NAME}")
+        await asyncio.sleep(wait_time)
+
+    trigger = IntervalTrigger(seconds=METHOD_SYNC_TIME)
+    plugin.bot.scheduler.add_job(method, trigger)
+    
+
+async def init_method():
+    pass
+
+@stopwatch(f"Task: Fetching and caching Anime Corner Ranking")
+async def method():
+    submission = await Reddit.get_anime_of_the_week_post()
+    pag = AnimeCornerPaginator2()
+    pag.submission = submission
+    pag.title = submission.title
+    url = pag.anime_corner_url
+    api = AnimeCornerAPI()
+    await api.fetch_ranking(url)
+
+def load(inu: Inu):
+    global bot
+    bot = inu
+    global METHOD_SYNC_TIME
+    METHOD_SYNC_TIME = inu.conf.commands.poll_sync_time
+    inu.add_plugin(plugin)
