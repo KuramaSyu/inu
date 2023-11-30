0ea772b71d4f03c0afacc42452971d280de7e1cd
KUR4M4 <52566613+zp33dy@users.noreply.github.com>
2023-11-30 22:05:07 +0100

    defered response before starting paginator

--- inu/ext/commands/tmdb.py
+++ inu/ext/commands/tmdb.py
@@ -23,18 +23,22 @@ bot: Inu
 @lightbulb.option("name", "name of tv show")
 @lightbulb.command("tv-show", "Search a tv show (TMDB)")
 @lightbulb.implements(commands.SlashCommand)
-async def search_show(ctx: context.Context):
+async def search_show(_ctx: context.Context):
+    ctx = get_context(_ctx.event)
+    await ctx.defer()
     pag = ShowPaginator()
-    await pag.start(get_context(ctx.event), ctx.options.name)
+    await pag.start(ctx, _ctx.options.name)
 
 
 @plugin.command
 @lightbulb.option("name", "name of movie")
 @lightbulb.command("movie", "Search a movie (TMDB)")
 @lightbulb.implements(commands.SlashCommand)
-async def search_movie(ctx: context.Context):
+async def search_movie(_ctx: context.Context):
+    ctx = get_context(_ctx.event)
+    await ctx.defer()
     pag = MoviePaginator()
-    await pag.start(get_context(ctx.event), ctx.options.name)
+    await pag.start(ctx, _ctx.options.name)
 
 
 
