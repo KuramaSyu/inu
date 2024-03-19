7b7f35a27aef138979f843d11f78b888c09726ef
kurama <52566613+zp33dy@users.noreply.github.com>
2024-03-19 19:32:08 +0100

    Update __init__.py

--- inu/utils/__init__.py
+++ inu/utils/__init__.py
@@ -1,6 +1,7 @@
 
 from .progress_bar import pacman
 from .shortcuts import *
+from .buttons import *
 from .colors import Colors
 from .string_crumbler import crumble, StringCutter, WordIterator
 from .language import Human, Multiple, get_date_format_by_timedelta
