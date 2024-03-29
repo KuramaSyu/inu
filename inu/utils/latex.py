c3f5aa62d54f4a85c94698c4c2eab4c87ab9ec3a
kurama <52566613+zp33dy@users.noreply.github.com>
2024-03-29 18:33:30 +0100

    improved qalc! to latex code

--- inu/utils/latex.py
+++ inu/utils/latex.py
@@ -33,9 +33,11 @@ class NumericStringParser(object):
     def pushFirst(self, strg, loc, toks):
         self.exprStack.append(toks[0])
 
-    def pushUMinus(self, strg, loc, toks):
+    def pushArray(self, strg, loc, toks):
         if toks and toks[0] == '-':
-            self.exprStack.append('unary -')
+            self.exprStack.append('-array')
+        else:
+            self.exprStack.append('array')
 
     def __init__(self):
         """
@@ -56,16 +58,17 @@ class NumericStringParser(object):
         x = Literal("x")
         fnumber = Combine(Word("+-" + nums, nums) +
                           Optional(point + Optional(Word(nums))) +
+                          Optional(Word("…")) +
                           Optional(e + Word("+-" + nums, nums)) + 
                           Optional(x)
         )
         ident = Word(alphas, alphas + "_$")
-        plus = Literal("+")
+        plus = Literal("+") 
         minus = Literal("-")
         mult = Literal("*") | Literal("×")
         div = Literal("/")
-        lpar = Literal("(").suppress()
-        rpar = Literal(")").suppress()
+        lpar = Literal("(")
+        rpar = Literal(")")
         sep = Literal(",").suppress() | Literal(";").suppress()
         
         addop = plus | minus
@@ -73,11 +76,14 @@ class NumericStringParser(object):
         expop = Literal("^")
         pi = CaselessLiteral("PI")
         equals = Literal("=") | Literal("≈")
+
         expr = Forward()
-        atom = ((Optional(oneOf("- +")) +
-                 ((ident + lpar + expr + ZeroOrMore(sep + expr) + rpar) | pi | e | fnumber | x).setParseAction(self.pushFirst))
-                | Optional(oneOf("- +")) + Group(lpar + expr + rpar)
-                ).setParseAction(self.pushUMinus)
+        parameter = expr + ZeroOrMore(sep + expr)
+        atom = (
+            (Optional(oneOf("- +")) +
+                ((ident + lpar + parameter + rpar) | pi | e | fnumber | x).setParseAction(self.pushFirst))
+            | (Optional(oneOf("- +")) + ZeroOrMore(Literal(" ")) + Group(lpar + expr + rpar)).setParseAction(self.pushArray)
+        )
         # by defining exponentiation as "atom [ ^ factor ]..." instead of
         # "atom [ ^ atom ]...", we get right-to-left exponents, instead of left-to-right
         # that is, 2^3^2 = 2^(3^2), not (2^3)^2.
@@ -103,6 +109,7 @@ class NumericStringParser(object):
             "×": operator.mul,
             "/": operator.truediv,
             "^": operator.pow}
+        # map function names to amount of parameters
         self.fn = {"sin": 1,
             "cos": 1,
             "tan": 1,
@@ -144,11 +151,11 @@ class NumericStringParser(object):
         elif fn_name == "sqrt":
             return "\\sqrt{{{0}}}"
         elif fn_name == "abs":
-            return "\\left|{0}\\right|"
+            return "\\left| {0} \\right|"
         elif fn_name == "trunc":
             return "\\text{{trunc}}({0})"
         elif fn_name == "round":
-            return "\\text{{round}}({0})"
+            return "\\text{{round}}\\left( {0} \\right)"
         elif fn_name == "sgn":
             return "\\text{{sgn}}{0}"
         elif fn_name == "sum":
@@ -167,8 +174,10 @@ class NumericStringParser(object):
             op2 = self.evaluateStack(s)
             op1 = self.evaluateStack(s)
             return f"{op1} {op} {op2}"
-        if op == 'unary -':
-            return -self.evaluateStack(s)
+        if op == 'array':
+            return f"\\left( {self.evaluateStack(s)} \\right)"
+        if op == '-array':
+            return f"- \\left( {self.evaluateStack(s)} \\right)"
         if op in "/":
             op2 = self.evaluateStack(s)
             op1 = self.evaluateStack(s)
@@ -188,9 +197,18 @@ class NumericStringParser(object):
         elif op in self.fn:
             format_values = [self.evaluateStack(s) for _ in range(self.fn[op])]
             return self.get_latex_fn(op).format(*format_values)
-            return self.fn[op](self.evaluateStack(s))
-        elif op[0].isalpha():
+        elif str(op[0]).isalpha():
             return str(op)
+        elif "…" in op:
+            part1, part2 = None, None
+            if " " in op:
+                # decimal partially periodic
+                part1, part2 = op.split(" ")
+                return f"{part1}\\overline{{{part2.replace('…', '')}}}"
+            else: 
+                # decimal fully periodic
+                part1, part2 = op.split(".")
+                return f"{part1}.\\overline{{{part2.replace('…', '')}}}"
         else:
             return str(op)
 
@@ -266,13 +284,40 @@ def evaluation2image(evaluation: str, multiline: bool = False) -> BytesIO:
     image = latex2image(latex, multiline=multiline)
     return image
 
+def prepare_for_latex(result: str) -> str:
+    """prepares the result for latex"""
+    result = result.replace("'", "") # remove number things for better readability
+    if len(result.splitlines()) > 1 and "warning" in result[0]:
+        result = result.split("\n")[-1] # remove warnings
+
+    old_to_new = {
+        "¹": "^1",
+        "²": "^2",
+        "³": "^3",
+        "⁴": "^4",
+        "⁵": "^5",
+        "⁶": "^6",
+        "⁷": "^7",
+        "⁸": "^8",
+        "⁹": "^9",
+        "⁰": "^0",
+        "−": "-",
+        "√": "sqrt",
+        "π": "pi",
+        "×": "*",
+        "÷": "/",
+        ":": "/",
+    }
+    for old, new in old_to_new.items():
+        result = result.replace(old, new)
+    return result
 
 if __name__ == "__main__":
     #latex = NumericStringParser().eval("sqrt(sum(5x, 1, 10))")
     #latex = NumericStringParser().eval("3 + 5 * 6 * sum(sqrt((9x + 16.9999x) / (2 * 7)),2,20) - 8 = 3")  # 10.0
     #latex = NumericStringParser().eval("20 × x × log(sqrt(3), e) = 10 × ln(3) × x ≈ x^integrate(3x, 0, 10)")
     try:
-      latex = NumericStringParser().eval("""(5 × x) = (sqrt(integrate(3 × x^2, 1, 5)) / 0.5) ≈""")
+      latex = NumericStringParser().eval(prepare_for_latex("""1 / 3 = 0.333…"""))
       image = latex2image(latex)
       img = Image.open(image)
       img.show()
