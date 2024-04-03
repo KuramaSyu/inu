c16664848c1fb2fd2df26fc49988fa97ad75f9ca
kurama <52566613+zp33dy@users.noreply.github.com>
2024-04-03 21:43:52 +0200

    Update __init__.py

--- inu/utils/__init__.py
+++ inu/utils/__init__.py
@@ -7,7 +7,7 @@ from .string_crumbler import crumble, StringCutter, WordIterator
 from .language import Human, Multiple, get_date_format_by_timedelta
 from .string_calculator import NumericStringParser, calc
 from .list_parser import ListParser
-from .latex import latex2image, evaluation2image
+from .latex import latex2image, evaluation2image, prepare_for_latex
 
 from .grid import Grid
 from .rest import *
