7f3ab2de64f86efcdebfded3a151cf952a188d19
kurama <52566613+zp33dy@users.noreply.github.com>
2024-04-28 23:15:58 +0200

    Improved logging of qalc update

--- inu/ext/tasks/qalc.py
+++ inu/ext/tasks/qalc.py
@@ -28,11 +28,16 @@ log = getLogger(__name__)
 loaded = False
 
 plugin = lightbulb.Plugin("Update qalc", "Sends daily automated Reddit pictures")
+i = 0
 
 async def update_qalc_currency():
     # -e = updating currency
-    result = await Bash.execute(["qalc", "-e", "x EUR = 1 BTC"])
-    log.debug(f"Updated qalc currency")
+    result = await Bash.execute(["qalc", "-t", "-e", "x EUR = 1 BTC"])
+    i += 1
+    if i % 5 == 0:
+        log.info(f"Updated qalculate currencies ({i}th time): {result}", prefix="Cache")
+    else:
+        log.debug(f"Updated qalculate currencies: {result}", prefix="Cache")
 
 @plugin.listener(ShardReadyEvent)
 async def load_tasks(event: ShardReadyEvent):
