2bab142af31018d1691417811c0a225904cecd47
kurama <52566613+zp33dy@users.noreply.github.com>
2024-05-27 02:20:40 +0200

    changed DB according to table changes

--- inu/utils/db/maths.py
+++ inu/utils/db/maths.py
@@ -1,5 +1,5 @@
-from datetime import datetime
-from typing import Dict, List
+from datetime import datetime, timedelta
+from typing import Dict, List, Tuple
 
 from core import Table, Database, getLogger
 
@@ -15,6 +15,7 @@ class MathScoreManager:
         user_id: int,
         stage: str,
         highscore: int,
+        time_needed: timedelta,
     ) -> bool:
         """
         inserts highscore if higher then previous
@@ -22,66 +23,68 @@ class MathScoreManager:
         Returns:
         -------
         `bool` : 
-            wehter or not highscore was updated
+            wehter or not highscore was inserted
         """
         table = Table("math_scores", debug_log=True)
-        user_stage = await table.select_row(["guild_id", "user_id", "stage"], [guild_id, user_id, stage])
-        if user_stage:
-            old_highscore = user_stage["highscore"]
-        else:
-            old_highscore = 0
-        if old_highscore < highscore:
-            await table.upsert(
-                ["guild_id", "user_id", "stage", "highscore", "date"],
-                values=[guild_id, user_id, stage, highscore, datetime.now()],
-                compound_of=3
-            )
-            return True
-        return False
+        await table.insert(
+            ["guild_id", "user_id", "stage", "highscore", "date", "time_needed"],
+            values=[guild_id, user_id, stage, highscore, datetime.now(), time_needed], 
+        )
+        return True
 
     @classmethod
     async def fetch_highscores(
-        cls,
-        type_: str,
-        guild_id: int,
-        user_id: int,
-    ):
-        """
-        Args:
-        -----
-        type_ : `str` 
-            the type [user|guild] (all from user or only user from guild)
-        guild_id : `int`
-        user_id : `int`
+            cls,
+            type_: str,
+            guild_id: int,
+            user_id: int,
+        ) -> Dict[str, List[Tuple[int, int, timedelta]]]:
+            """
+            Fetches highscores based on the given type, guild ID, and user ID.
 
+            Args:
+            -----
+            type_ : str
+                The type of highscores to fetch. Valid values are "user" or "guild".
+            guild_id : int
+                The ID of the guild.
+            user_id : int
+                The ID of the user.
 
-        Returns:
-        --------
-        `Dict[str, List[Dict[int, int]]]` 
-            Mapping form stage to highscore list containing
-            Dict mapping from user_id to highscore
-            -> `Dict[stage, ListDesc[Dict[user_id, highscore]]]`
-        """
-        table = Table("math_scores")
-        if type_ == "user":
-            records = await table.select(
-                ["guild_id", "user_id"],
-                [guild_id, user_id],
-                order_by="highscore DESC",
-            )
-        elif type_ == "guild":
-            records = await table.select(
-                ["guild_id"],
-                [guild_id],
-                order_by="highscore DESC",
-            )
-        else:
-            raise RuntimeError(f"{type_=} is unvalid")
-        stages: Dict[str, List[Dict[int, int]]] = {}
-        for r in records:
-            stage = r["stage"]
-            stage_list = stages.get(stage, [])
-            stage_list.append({r["user_id"]: r["highscore"]})
-            stages[stage] = stage_list
-        return stages
+            Returns:
+            --------
+            Dict[str, List[Tuple[int, int, timedelta]]]
+                A dictionary mapping from stage to a list of tuples containing user ID, highscore, and time per task.
+
+            Raises:
+            ------
+            RuntimeError:
+                If an invalid type is provided.
+
+            """
+            table = Table("math_scores")
+            if type_ == "user":
+                records = await table.select(
+                    ["guild_id", "user_id"],
+                    [guild_id, user_id],
+                    select="*, time_needed / highscore as time_per_task",
+                    order_by="highscore DESC",
+                )
+            elif type_ == "guild":
+                records = await table.select(
+                    ["guild_id"],
+                    [guild_id],
+                    select="*, time_needed / highscore as time_per_task",
+                    order_by="highscore DESC",
+                )
+            else:
+                raise RuntimeError(f"{type_=} is invalid")
+            # mapping from stage to highscore list containing user_id, highscore and time_per_task
+            stages: Dict[str, List[Tuple[int, int, float]]] = {}
+            for r in records:
+                stage = r["stage"]
+                stage_list = stages.get(stage, [])
+                stage_list.append((r["user_id"], r["highscore"], r["time_per_task"]))
+                stages[stage] = stage_list
+            return stages
         
