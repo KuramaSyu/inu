f12d5ed6ff6f375d1586bb3f61772243bb6d835c
kurama <52566613+zp33dy@users.noreply.github.com>
2024-10-06 16:58:14 +0200

    Create response_lock.py

---
+++ inu/ext/commands/music_utils/response_lock.py
@@ -0,0 +1,32 @@
+from typing import *
+from datetime import datetime, timedelta
+
+import asyncio
+
+
+class ResponseLock:
+    """
+    A Lock to prevent the MusicPlayer to send 2 responses whithin a short time interval.
+    """
+    def __init__(self, minimum_delay: timedelta) -> None:
+        self._minimum_delay: timedelta = minimum_delay
+        self._last_response: datetime = datetime(2000, 0 , 0)
+        
+    def is_available(self) -> bool:
+        """
+        Wheter or not the lock is available.
+        """
+        return datetime.now() - self._last_response >= self._minimum_delay
+    
+    def lock(self) -> bool:
+        """
+        Lock the lock.
+        
+        Returns:
+            bool: Wheter or not the lock was locked.
+        """
+        if not self.is_available():
+            return False
+        self._last_response = datetime.now()
+        return True
+        
