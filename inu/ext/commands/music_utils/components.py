d07f5342cf821ee760acc208210946654e4b6a44
kurama <52566613+zp33dy@users.noreply.github.com>
2024-10-06 17:49:46 +0200

    Create components.py

---
+++ inu/ext/commands/music_utils/components.py
@@ -0,0 +1,79 @@
+from typing import *
+from hikari.impl import MessageActionRowBuilder
+
+
+class MusicMessageComponents:
+    def __init__(self, player: "MusicPlayer"):
+        self._player = player
+        self._disable_all = False
+        
+    @classmethod
+    def from_player(cls, player: "MusicPlayer"):
+        return cls(player)
+    
+    def disable_all(self):
+        self._disable_all = True
+        return self
+    
+    def build_components(
+        self,
+    ) -> List[hikari.impl.MessageActionRowBuilder]:
+        """builds the components for the music message
+        
+        Args:
+        ----
+        disable_all : bool=False
+            If all buttons should be disabled, by default False
+        """
+        is_paused = self.player.is_paused
+        paused_or_stopped = False
+        disable_all = self._disable_all
+        if not disable_all:
+            node = self.player._node
+            if not node:
+                disable_all = True
+
+            
+        action_rows = [
+            (
+                MessageActionRowBuilder()
+                .add_interactive_button(
+                    hikari.ButtonStyle.SECONDARY, 
+                    "music_skip_1",
+                    is_disabled=disable_all or is_paused,
+                    emoji="1️⃣",
+                )
+                .add_interactive_button(
+                    hikari.ButtonStyle.SECONDARY,
+                    "music_skip_2",
+                    emoji="2️⃣",
+                    is_disabled=disable_all or is_paused,
+                )
+                .add_interactive_button(
+                    hikari.ButtonStyle.SECONDARY,
+                    "music_shuffle",
+                    emoji="🔀",
+                    is_disabled=disable_all,
+                )
+                .add_interactive_button(
+                    hikari.ButtonStyle.SECONDARY,
+                    "music_stop",
+                    emoji="🛑",
+                    is_disabled=disable_all,
+                )
+            )
+        ]
+        if not disable_all:
+            if is_paused:
+                action_rows[0].add_interactive_button(
+                    hikari.ButtonStyle.PRIMARY,
+                    "music_resume",
+                    emoji="▶",
+                )
+            else:
+                action_rows[0].add_interactive_button(
+                    hikari.ButtonStyle.SECONDARY,
+                    "music_pause",
+                    emoji="⏸",
+                )
+        return action_rows
