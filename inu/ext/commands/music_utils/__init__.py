659e369121a420f84dc3b6466ae3643b4548fed3
kurama <52566613+zp33dy@users.noreply.github.com>
2024-10-06 17:58:58 +0200

    add MusicMessageComponents

--- inu/ext/commands/music_utils/__init__.py
+++ inu/ext/commands/music_utils/__init__.py
@@ -1,4 +1,5 @@
 from .helpers import *
 from .response_lock import ResponseLock
+from .components import MusicMessageComponents
 from .lavalink_voice import LavalinkVoice, LavalinkClient, TrackUserData
 from .player import MusicPlayer, MusicPlayerManager
