6d5b0193895a4c1414cf05e645e8a6a571e14f8a
kurama <52566613+zp33dy@users.noreply.github.com>
2024-10-06 16:58:11 +0200

    add ResponseLock

--- inu/ext/commands/music_utils/__init__.py
+++ inu/ext/commands/music_utils/__init__.py
@@ -1,3 +1,4 @@
 from .helpers import *
+from .response_lock import ResponseLock
 from .lavalink_voice import LavalinkVoice, LavalinkClient, TrackUserData
 from .player import MusicPlayer, MusicPlayerManager
