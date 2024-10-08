4bb2ba29239c1c9256c8aa6383485b7d0d1deb60
kurama <52566613+zp33dy@users.noreply.github.com>
2024-10-08 10:06:54 +0200

    Create constants.py

---
+++ inu/ext/commands/music_utils/constants.py
@@ -0,0 +1,9 @@
+import re
+
+# prefix for autocomplete history values
+HISTORY_PREFIX = "History: "
+MEDIA_TAG_PREFIX = "Media Tag: "
+# if the bot is alone in a channel, then
+DISCONNECT_AFTER = 60 * 10  # seconds
+# REGEX for [title](url) markdown
+MARKDOWN_URL_REGEX = re.compile(r"\[(.*?)\]\((https:\/\/.*?)\)")
