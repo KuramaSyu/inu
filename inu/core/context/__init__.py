ac791e1fe5169b5723ccf928597ffaca98ea135b
kurama <52566613+KuramaSyu@users.noreply.github.com>
2024-12-03 21:28:42 +0100

    fix: resolved circular import

--- inu/core/context/__init__.py
+++ inu/core/context/__init__.py
@@ -1,13 +1,16 @@
-from .protocols import InuContext, InuContextProtocol
-from .base import InuContextBase, UniqueContextInstance, Response
 from .mixins import (
     AuthorMixin, CustomIDMixin, GuildsAndChannelsMixin, HasChannelLikeInteraction, 
     HasApp, HasInteraction, Interaction
 )
+
+
+from .base import InuContextBase, UniqueContextInstance, Response
+
 from .response import (
     BaseResponseState, CreatedResponseState, DeferredCreateResponseState, 
     DeletedResponseState, InitialResponseState
 )
+from .protocols import InuContext, InuContextProtocol
 from .interactions import CommandInteractionContext, InteractionContext
 #from .rest import RESTContext
 from .factory import get_context
