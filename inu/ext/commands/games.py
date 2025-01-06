2488637b3718a556aadb96e6fdcc18b727145d7c
kurama <52566613+KuramaSyu@users.noreply.github.com>
2025-01-06 16:38:14 +0100

    temp

--- inu/ext/commands/games.py
+++ inu/ext/commands/games.py
@@ -1,226 +1,224 @@
+from os import name
+from re import T
 import traceback
 from typing import *
-import asyncio
-import json
+import aiohttp
 
-import hikari
+from hikari import Member, Snowflake, User
 from hikari.impl import MessageActionRowBuilder
 import lightbulb
-import lightbulb.context as context
-from lightbulb import commands, cooldown_algorithms
-from lightbulb.commands import OptionModifier as OM
-import aiohttp
+from lightbulb import Context, Loader, SlashCommand, invoke, Group
+from lightbulb.prefab import sliding_window
+
 
 from utils.games.connect_four_handler import (
     Connect4Handler, 
-    Connect4FallingRowsHandler, 
+    Connect4FallingRowsHandler,
+    Player, 
     get_handler_from_letter, 
     MemoryConnect4Handler,
     RandomTerrainConnect4Handler,
 )
 from utils.games import HikariOnu
 from utils import AkinatorSI, Human
-from core import getLogger, Inu, get_context
-
+from core import getLogger, Inu, get_context, InuContext
 
 log = getLogger(__name__)
-Context = Union[context.SlashContext, context.PrefixContext]
-plugin = lightbulb.Plugin("Game Commands", "Extends the commands with commands all about games")
+loader = lightbulb.Loader()
+bot: Inu = Inu.instance
 onu_sessions = set()
-bot: Inu
+
+def cast_to_members(p1: User, p2: Optional[User], guild: Snowflake | None) -> Tuple[Member, Optional[Member]]:
+    """casts `Tuple[User, Optional[User]]` to `Tuple[Member, Optional[Member]]`"""
+    assert guild is not None
+    player1 = bot.cache.get_member(guild, p1)
+    assert player1 is not None
+    player2 = bot.cache.get_member(guild, p2) if p2 else None
+    return (player1, player2)
 
 
-@plugin.listener(hikari.InteractionCreateEvent)
-async def on_connect4_restart(event: hikari.InteractionCreateEvent):
-    game_modifier = ""
-    if not isinstance(event.interaction, hikari.ComponentInteraction):
-        return
-    try:
-        # extract data from custom_id or exit
-        custom_id_json = json.loads(event.interaction.custom_id)
-        custom_id: str = custom_id_json["type"]
-        assert custom_id.startswith("c4") # c4{game_letter}-{rows}x{columns}
-        game_modifier = custom_id[2]
-        rxc: List[str] = custom_id[4:].split("x")
-        rows = int(rxc[0])
-        columns = int(rxc[1])
-        guild_id = custom_id_json["gid"]
-
-        p1 = await bot.mrest.fetch_member(guild_id, custom_id_json["p1"])
-        p2 = await bot.mrest.fetch_member(guild_id, custom_id_json["p2"])
-    except:
-        return
-    handler_class = get_handler_from_letter(game_modifier)
-    handler = handler_class(p1, p2, rows=rows, columns=columns)
-    ctx = get_context(event)
-    await ctx.respond(
-        components=[
-            MessageActionRowBuilder()
-            .add_interactive_button(
-                hikari.ButtonStyle.PRIMARY, 
-                "connect4-activated-restart",
-                emoji="🔁"
-            )
-        ],
-        update=True
-    )
-    await handler.start(ctx)
-
-    
-async def start_connect_4(ctx: Context, rows: int, columns: int, handler: Type[Connect4Handler] | None = None, **kwargs):
-    if not ctx._options.get("player2"):
-        ctx._options["player2"] = ctx.member
+# Shared functions
+async def start_connect_4(
+    ctx: InuContext, 
+    rows: int, 
+    columns: int, 
+    players: Tuple[Member, Optional[Member]],
+    handler: Type[Connect4Handler] | None = None, 
+    **kwargs
+):
+    if not players[1]:
+        member = ctx.member
+        assert member is not None
+        players = (players[0], member)
     handler = handler or Connect4Handler
-    h = handler(ctx.options.player1, ctx.options.player2, rows=rows, columns=columns, **kwargs)
+    h = handler(players[0], players[1], rows=rows, columns=columns, **kwargs)  # type: ignore
     await h.start(ctx)
 
 
+con4 = Group(name="Connect4", description="Various Connect 4 games", dm_enabled=False)
+# Connect 4 base command group
 
-@plugin.command
-@lightbulb.add_checks(lightbulb.guild_only)
-@lightbulb.add_cooldown(30, 2, lightbulb.UserBucket)
-@lightbulb.option("player2", "The second player - DEFAULT: you", type=hikari.Member, default=None)
-@lightbulb.option("player1", "A player\nNOTE: ping like @user", type=hikari.Member)
-@lightbulb.command("connect-4", "starts a Connect 4 game")
-@lightbulb.implements(commands.PrefixCommandGroup, commands.SlashCommandGroup)
-async def connect4(ctx: Context):
-    ...
+@con4.register
+class ClassicConnect4(
+    SlashCommand,
+    name = "classic",
+    description = "Starts a classic 6x7 Connect 4 game"
+):
+    player1 = lightbulb.user("player1", "The first player")
+    player2 = lightbulb.user("player2", "The second player", default=None)
 
-
-
-@connect4.child
-@lightbulb.add_checks(lightbulb.guild_only)
-@lightbulb.add_cooldown(30, 2, lightbulb.UserBucket)
-@lightbulb.option("player2", "The second player - DEFAULT: you", type=hikari.Member, default=None)
-@lightbulb.option("player1", "A player\nNOTE: ping like @user", type=hikari.Member)
-@lightbulb.command("classic", "starts a Connect 4 game", aliases=["6x7"])
-@lightbulb.implements(commands.PrefixSubCommand, commands.SlashSubCommand)
-async def connect4_classic(ctx: Context):
-    await start_connect_4(ctx, rows=6, columns=7)
+    @invoke
+    async def callback(self, _: Context, ctx: InuContext):
+        players = cast_to_members(self.player1, self.player2, ctx.guild_id)
+        await start_connect_4(ctx, rows=6, columns=7, players=players)
 
 
+@con4.register
+class SquareConnect4(
+    SlashCommand,
+    name="square",
+    description="Starts an 8x8 Connect 4 game"
+):
+    player1 = lightbulb.user("player1", "The first player")
+    player2 = lightbulb.user("player2", "The second player", default=None)
 
-@connect4.child
-@lightbulb.add_checks(lightbulb.guild_only)
-@lightbulb.add_cooldown(30, 2, lightbulb.UserBucket)
-@lightbulb.option("player2", "The second player - DEFAULT: you", type=hikari.Member, default=None)
-@lightbulb.option("player1", "A player\nNOTE: ping like @user", type=hikari.Member)
-@lightbulb.command("square", "starts a Connect 4 game")
-@lightbulb.implements(commands.PrefixSubCommand, commands.SlashSubCommand)
-async def connect4_8by8(ctx: Context):
-    await start_connect_4(ctx, rows=8, columns=8)
+    @invoke
+    async def callback(self, _: lightbulb.Context, ctx: InuContext):
+        players = cast_to_members(self.player1, self.player2, ctx.guild_id)
+        await start_connect_4(ctx, rows=8, columns=8, players=players)
+
+@con4.register
+class FallingRowsConnect4(
+    SlashCommand,
+    name="falling-rows",
+    description="A game of Connect 4 with Tetris-like falling rows"
+):
+    player1 = lightbulb.user("player1", "The first player")
+    player2 = lightbulb.user("player2", "The second player", default=None)
+
+    @invoke
+    async def callback(self, _: lightbulb.Context, ctx: InuContext):
+        players = cast_to_members(self.player1, self.player2, ctx.guild_id)
+        await start_connect_4(ctx, rows=6, columns=7, handler=Connect4FallingRowsHandler, players=players)
+
+@con4.register
+class MemoryConnect4(
+    SlashCommand,
+    name="memory",
+    description="A Connect 4 game with hidden tokens"
+):
+
+    player1 = lightbulb.user("player1", "The first player")
+    player2 = lightbulb.user("player2", "The second player", default=None)
+    unmemory_count = lightbulb.integer("unmemory-count", "Rounds before tokens are visible again", default=5)
+
+    @invoke
+    async def callback(self, _: lightbulb.Context, ctx: InuContext):
+        players = cast_to_members(self.player1, self.player2, ctx.guild_id)
+        await start_connect_4(ctx, rows=6, columns=7, handler=MemoryConnect4Handler, unmemory=self.unmemory_count, players=players)
+
+@con4.register
+class RandomTerrainConnect4(
+    SlashCommand,
+    name="random-terrain",
+    description="A Connect 4 game with a random start terrain"
+):
+    player1 = lightbulb.user("player1", "The first player")
+    player2 = lightbulb.user("player2", "The second player", default=None)
+
+    @invoke
+    async def callback(self, _: lightbulb.Context, ctx: InuContext):
+        players = cast_to_members(self.player1, self.player2, ctx.guild_id)
+        await start_connect_4(ctx, rows=6, columns=7, handler=RandomTerrainConnect4Handler, players=players)
 
 
-
-@connect4.child
-@lightbulb.add_checks(lightbulb.guild_only)
-@lightbulb.add_cooldown(30, 2, lightbulb.UserBucket)
-@lightbulb.option("player2", "The second player - DEFAULT: you", type=hikari.Member, default=None)
-@lightbulb.option("player1", "A player\nNOTE: ping like @user", type=hikari.Member)
-@lightbulb.command("falling-rows", "A game of Connect 4 which drops rows Tetris like")
-@lightbulb.implements(commands.PrefixSubCommand, commands.SlashSubCommand)
-async def connect4_falling_rows(ctx: Context):
-    await start_connect_4(ctx, rows=6, columns=7, handler=Connect4FallingRowsHandler)
-
-
-
-@connect4.child
-@lightbulb.add_checks(lightbulb.guild_only)
-@lightbulb.add_cooldown(30, 2, lightbulb.UserBucket)
-@lightbulb.option("unmemory-count", "each <unmemory-count> round you will see the normal board", type=int, default=5)
-@lightbulb.option("player2", "The second player - DEFAULT: you", type=hikari.Member, default=None)
-@lightbulb.option("player1", "A player\nNOTE: ping like @user", type=hikari.Member)
-@lightbulb.command("memory", "A game of Connect 4 where you can't distinguish the tokens")
-@lightbulb.implements(commands.PrefixSubCommand, commands.SlashSubCommand)
-async def connect4_memory(ctx: Context):
-    await start_connect_4(ctx, rows=6, columns=7, handler=MemoryConnect4Handler, unmemory=ctx.options["unmemory-count"])
-
-
-@connect4.child
-@lightbulb.add_checks(lightbulb.guild_only)
-@lightbulb.add_cooldown(30, 2, lightbulb.UserBucket)
-@lightbulb.option("player2", "The second player - DEFAULT: you", type=hikari.Member, default=None)
-@lightbulb.option("player1", "A player\nNOTE: ping like @user", type=hikari.Member)
-@lightbulb.command("random-terrain", "A game of Connect 4 with a random start terrain")
-@lightbulb.implements(commands.PrefixSubCommand, commands.SlashSubCommand)
-async def connect4_memory(ctx: Context):
-    await start_connect_4(ctx, rows=6, columns=7, handler=RandomTerrainConnect4Handler)
-
-
-
-@plugin.command
-@lightbulb.add_checks(lightbulb.guild_only)
-@lightbulb.add_cooldown(120, 2, lightbulb.UserBucket)
-@lightbulb.option("player8", "The 8th @player", type=hikari.Member, default=None)
-@lightbulb.option("player7", "The 7th @player", type=hikari.Member, default=None)
-@lightbulb.option("player6", "The 6th @player", type=hikari.Member, default=None)
-@lightbulb.option("player5", "The 5th @player", type=hikari.Member, default=None)
-@lightbulb.option("player4", "The 4th @player", type=hikari.Member, default=None)
-@lightbulb.option("player3", "The third @player", type=hikari.Member, default=None)
-@lightbulb.option("player2", "The second @player", type=hikari.Member)
-@lightbulb.option("player1", "The first @player", type=hikari.Member)
-@lightbulb.command("onu", "starts a onu game")
-@lightbulb.implements(commands.PrefixCommand, commands.SlashCommand)
-async def onu(ctx: Context):
-    players: Dict[int, hikari.Member] = {p.id: p for p in ctx._options.values() if not p is None}
-    if not ctx.author.id in players.keys():
-        return await ctx.respond("You can't start a game without you")
-    for p_id, p in players.items():
-        if p_id in onu_sessions:
-            return await ctx.respond(
-                f"There is currently a game running with {p.mention}, hence you can't start a game"
-            )
-        else:
-            onu_sessions.add(p_id)
-    await ctx.respond(f"Starting Onu game with {Human.list_([p.display_name for p in players.values()], with_a_or_an=False)}")
-    onu = HikariOnu(players)
-    try:
-        await onu.start(ctx.bot, ctx)
-    except Exception:
-        log.error(traceback.format_exc())
-    finally:
-        for p_id in players.keys():
-            onu_sessions.remove(p_id)
+loader.command(con4)
 
 
 
-@plugin.command
-@lightbulb.add_cooldown(60, 1, lightbulb.UserBucket)
-@lightbulb.command("akinator", "I guess a character for you", aliases=["aki"], auto_defer=True)
-@lightbulb.implements(commands.SlashCommand, commands.PrefixCommand)
-async def akinator(ctx: Context):
-    aki = AkinatorSI("en")
-    await aki.start(ctx)
+# @loader.command
+# class OnuGame(
+#     SlashCommand,
+#     name="onu",
+#     description="Starts an ONU game",
+#     dm_enabled=False,
+#     default_member_permissions=None
+# ):
+#     player1 = lightbulb.user("player1", "The first player")
+#     player2 = lightbulb.user("player2", "The second player")
+#     player3 = lightbulb.user("player3", "The third player", default=None)
+#     player4 = lightbulb.user("player4", "The fourth player", default=None)
+#     player5 = lightbulb.user("player5", "The fifth player", default=None)
+#     player6 = lightbulb.user("player6", "The sixth player", default=None)
+#     player7 = lightbulb.user("player7", "The seventh player", default=None)
+#     player8 = lightbulb.user("player8", "The eighth player", default=None)
+
+#     @invoke
+#     async def callback(self, _: lightbulb.Context, ctx: InuContext):
+#         players: List[Member] = [ctx.bot.cache.get_member(ctx.guild, v) for k, v in self.__dict__.items() if "player" in k]  # type:ignore
+#         player_map = {p.id: p for p in players if p is not None}
+#         if ctx.author.id not in players:
+#             await ctx.respond("You can't start a game without yourself")
+#             return
+
+#         for p_id, p in player_map.items():
+#             if p_id in onu_sessions:
+#                 await ctx.respond(f"There is a running game with {p.mention}. Cannot start a new game.")
+#                 return
+
+#         for p_id in players:
+#             onu_sessions.add(p_id)
+
+#         await ctx.respond(f"Starting Onu game with {Human.list_([p.display_name for p in player_map.values()], with_a_or_an=False)}")
+#         onu = HikariOnu(player_map)  # type:ignore
+
+#         try:
+#             await onu.start(ctx.bot, ctx)
+#         except Exception:
+#             log.error(traceback.format_exc())
+#         finally:
+#             for p_id in players:
+#                 onu_sessions.remove(p_id)
+
+
+# @loader.command
+# class AkinatorGame(
+#     SlashCommand,
+#     name="akinator",
+#     description="Guess a character with Akinator",
+#     dm_enabled=False,
+# ):
+#     @invoke
+#     async def callback(self, ctx: lightbulb.Context):
+#         aki = AkinatorSI("en")
+#         await aki.start(ctx)
 
 
 REVERSI_BASE_URL = "http://inuthebot.duckdns.org:8888"
 CREATE_SESSION_ENDPOINT = f"{REVERSI_BASE_URL}/create_session"
 
-@plugin.command
-@lightbulb.add_cooldown(10, 1, lightbulb.UserBucket)
-@lightbulb.command("reversi", "Creates a session for reversi")
-@lightbulb.implements(commands.SlashCommand, commands.PrefixCommand)
-async def reversi(ctx: Context):
-    ctx = get_context(ctx.event)
-    # make a get request to inuthebot.duckdns.org:8888/create_session
-    async with aiohttp.ClientSession() as session:
-        async with session.get(CREATE_SESSION_ENDPOINT, ssl=False) as resp:
-            if resp.status != 200:
-                return await ctx.respond("Something went wrong", ephemeral=True)
-            data = await resp.json()
-            await ctx.respond(
-                component=(
-                    MessageActionRowBuilder()
-                    .add_link_button(
-                        data['data']["link"], 
-                        label=f"Reversi Lobby Code: {data['data']['code']}"  
-                    )
-                )          
-            )
+# @loader.command
+# class ReversiGame(lightbulb.SlashCommand):
+#     name = "reversi"
+#     description = "Creates a session for Reversi"
+#     dm_enabled = False
+#     default_member_permissions = None
 
+#     @invoke
+#     async def callback(self, ctx: lightbulb.Context):
+#         async with aiohttp.ClientSession() as session:
+#             async with session.get(CREATE_SESSION_ENDPOINT, ssl=False) as resp:
+#                 if resp.status != 200:
+#                     await ctx.respond("Something went wrong", ephemeral=True)
+#                     return
 
-
-def load(inu: lightbulb.BotApp):
-    global bot
-    bot = inu
-    inu.add_plugin(plugin)
+#                 data = await resp.json()
+#                 await ctx.respond(
+#                     component=(
+#                         MessageActionRowBuilder()
+#                         .add_link_button(
+#                             data['data']["link"],
+#                             label=f"Reversi Lobby Code: {data['data']['code']}"
+#                         )
+#                     )
+#                 )
