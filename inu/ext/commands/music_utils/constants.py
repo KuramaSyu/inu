b4d16a66667adf0c7e472c77743e7e54b1debd1e
kurama <52566613+KuramaSyu@users.noreply.github.com>
2025-01-13 22:43:25 +0100

    chore: updated regex for urls

--- inu/ext/commands/music_utils/constants.py
+++ inu/ext/commands/music_utils/constants.py
@@ -7,4 +7,8 @@ MEDIA_TAG_PREFIX = "Media Tag: "
 DISCONNECT_AFTER = 60 * 10  # seconds
 # REGEX for [title](url) markdown
 MARKDOWN_URL_REGEX = re.compile(r"\[(.*?)\]\((https:\/\/.*?)\)")
-URL_REGEX = re.compile(r"(https:\/\/.*?)")
+URL_REGEX = re.compile(
+    r'http[s]?://'                # HTTP or HTTPS
+    r'(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|'  # Allowed characters
+    r'(?:%[0-9a-fA-F][0-9a-fA-F]))+'  # Percent-encoded characters
+)
