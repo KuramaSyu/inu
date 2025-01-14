a1aa1fa62326a69120cea8ce2895ae566163a188
kurama <52566613+KuramaSyu@users.noreply.github.com>
2025-01-14 21:46:30 +0100

    refactor: lb v2 -> v3

--- inu/ext/commands/reminders.py
+++ inu/ext/commands/reminders.py
@@ -17,7 +17,7 @@ from hikari.impl.special_endpoints import MessageActionRowBuilder
 from hikari.events import InteractionCreateEvent
 import lightbulb
 import lightbulb.utils as lightbulb_utils
-from lightbulb import Context
+from lightbulb import Context, SlashCommand, invoke
 import hikari
 from numpy import isin
 import apscheduler
@@ -25,7 +25,7 @@ from apscheduler.triggers.interval import IntervalTrigger
 
 from utils.db.reminders import REMINDER_UPDATE, Reminders
 from utils import HikariReminder, Human, Paginator, crumble
-from core import Table
+from core import Table, Inu, InuContext
 
 
 from core import getLogger
@@ -33,49 +33,8 @@ from core import getLogger
 
 log = getLogger(__name__)
 
-plugin = lightbulb.Plugin("Basics", "Extends the commands with basic commands", include_datastore=True)
-
-        
-
-@plugin.command
-@lightbulb.add_cooldown(300, 3, lightbulb.UserBucket)
-@lightbulb.option(
-    "info", 
-    "the waiting time, continued by the text you want to be reminded", 
-    type=str, 
-    modifier=OM.CONSUME_REST,
-)
-@lightbulb.command("remind", "set a reminder", aliases=["timer"])
-@lightbulb.implements(commands.PrefixCommand, commands.SlashCommand)
-async def create_reminder(ctx: context.Context):
-    message_id = 0
-    if hasattr(ctx.event, "message_id"):
-        message_id = ctx.event.message_id
-    offset_hours = await fetch_hour_offset(ctx.guild_id or ctx.author.id)
-    reminder = HikariReminder(
-        ctx.channel_id,
-        ctx.author.id,
-        message_id,
-        ctx.options.info,
-        ctx,
-        offset_hours=offset_hours,
-    )
-    await ctx.respond(
-        f"reminding you to: <t:{str(int(reminder.datetime.timestamp()))}>\n"\
-        f"or in seconds: `{Human.number(round(reminder.in_seconds, 2))}`"
-    )
-
-@create_reminder.set_error_handler
-async def on_reminder_error(event: CommandErrorEvent):
-    
-    with open("inu/data/text/reminder-help.txt", "r", encoding="utf-8") as f:
-        txt = f.read()
-    log.warning(f"Error in reminder command: {traceback.format_exc()}")
-    if event.context.options.info is None:
-        await event.context.respond(txt)
-    else:
-        await event.context.respond(f"I am not a fucking trash converter 🖕\n\n{txt}")
-    return True
+loader = lightbulb.Loader()
+bot: Inu = Inu.instance
 
 async def fetch_hour_offset(id: int):
     table = Table("guild_timezones")
@@ -85,51 +44,100 @@ async def fetch_hour_offset(id: int):
     except IndexError:
         return 0
 
-@plugin.command
-@lightbulb.command("reminder", "Group with options for reminders")
-@lightbulb.implements(commands.SlashCommandGroup, commands.PrefixCommandGroup)
-async def reminder(ctx: context.Context):
-    pass
-
-@reminder.child
-@lightbulb.add_cooldown(300, 8, lightbulb.UserBucket)
-@lightbulb.command("list", "Get a list with all your reminders")
-@lightbulb.implements(commands.SlashSubCommand, commands.PrefixSubCommand)
-async def reminder_list(ctx: context.Context):
-    table = Table("reminders")
-    records = await table.select(
-        columns=["creator_id"],
-        matching_values=[ctx.author.id],
-        order_by="remind_time ASC"
-    )
-    if records is None:
-        await ctx.respond("There are no upcoming reminders")
-        return
-    msg = f"**{ctx.author.username}'s reminders**\n\n>>> "
-    for r in records:
-        msg += (
-            f"ID: {r['reminder_id']} "
-            f"<t:{int(r['remind_time'].timestamp())}:R> <t:{int(r['remind_time'].timestamp())}>\n"
-        )
-        if r["remind_text"]:
-            msg += f"``` {Human.short_text(r['remind_text'], 70)} ```"
-        msg += "\n\n"
-    pag = Paginator(page_s=crumble(msg))
-    await pag.start(ctx)
-
-@reminder.child
-@lightbulb.add_cooldown(300, 5, lightbulb.UserBucket)
-@lightbulb.option("id", "The id (get it with reminder list) of the reminder", type=int)
-@lightbulb.command("cancel", "cancel a reminder", aliases=["delete"])
-@lightbulb.implements(commands.PrefixSubCommand, commands.SlashSubCommand)
-async def reminder_cancel(ctx: context.Context):
-    record = await Reminders.delete_reminder_by_id(int(ctx.options.id))
-    if not record:
-        await ctx.respond(f"I would do it, but there is no reminder with id {ctx.options.id}")
-    elif record["creator_id"] != ctx.author.id:
-        await ctx.respond(f"_YOU_ haven't a reminder with that id. So I won't cancel it!")
+@loader.error_handler
+async def handler(exc: lightbulb.exceptions.ExecutionPipelineFailedException) -> bool:
+    ctx = exc.context
+    if isinstance(exc.causes[0], RuntimeError):
+        with open("inu/data/text/reminder-help.txt", "r", encoding="utf-8") as f:
+            txt = f.read()
+        log.warning(f"Error in reminder command: {traceback.format_exc()}")
+        await ctx.respond(f"{txt}")
+        return True
     else:
-        await ctx.respond(f"Reminder is canceled :)")
+        return False
+    
+    
 
-def load(bot: lightbulb.BotApp):
-    bot.add_plugin(plugin)
+@loader.command
+class Remind(
+    SlashCommand,
+    name="remind",
+    description="set a reminder",
+    dm_enabled=True
+):
+    info = lightbulb.string(
+        "info",
+        "the waiting time, continued by the text you want to be reminded",
+    )
+
+    @invoke
+    async def callback(self, _: lightbulb.Context, ctx: InuContext):
+        message_id = 0
+        if ctx.message_id:
+            message_id = ctx.message_id
+        offset_hours = await fetch_hour_offset(ctx.guild_id or ctx.author.id)
+        reminder = HikariReminder(
+            ctx.channel_id,
+            ctx.author.id,
+            message_id,
+            self.info,
+            ctx,
+            offset_hours=offset_hours,
+        )
+        await ctx.respond(
+            f"reminding you to: <t:{str(int(reminder.datetime.timestamp()))}>\n"\
+            f"or in seconds: `{Human.number(round(reminder.in_seconds, 2))}`"
+        )
+
+reminder_group = lightbulb.Group(name="reminder", description="Group with options for reminders")
+
+@reminder_group.register
+class ReminderList(
+    SlashCommand,
+    name="list",
+    description="Get a list with all your reminders",
+    dm_enabled=True
+):
+    @invoke
+    async def callback(self, _: lightbulb.Context, ctx: InuContext):
+        table = Table("reminders")
+        records = await table.select(
+            columns=["creator_id"],
+            matching_values=[ctx.author.id],
+            order_by="remind_time ASC"
+        )
+        if records is None:
+            await ctx.respond("There are no upcoming reminders")
+            return
+        msg = f"**{ctx.author.username}'s reminders**\n\n>>> "
+        for r in records:
+            msg += (
+                f"ID: {r['reminder_id']} "
+                f"<t:{int(r['remind_time'].timestamp())}:R> <t:{int(r['remind_time'].timestamp())}>\n"
+            )
+            if r["remind_text"]:
+                msg += f"``` {Human.short_text(r['remind_text'], 70)} ```"
+            msg += "\n\n"
+        pag = Paginator(page_s=crumble(msg))
+        await pag.start(ctx)
+
+@reminder_group.register
+class ReminderCancel(
+    SlashCommand,
+    name="cancel",
+    description="cancel a reminder",
+    dm_enabled=True
+):
+    reminder_id = lightbulb.integer("id", "The id (get it with reminder list) of the reminder")
+
+    @invoke
+    async def callback(self, _: lightbulb.Context, ctx: InuContext):
+        record = await Reminders.delete_reminder_by_id(int(self.reminder_id))
+        if not record:
+            await ctx.respond(f"I would do it, but there is no reminder with id {self.reminder_id}")
+        elif record["creator_id"] != ctx.author.id:
+            await ctx.respond(f"_YOU_ haven't a reminder with that id. So I won't cancel it!")
+        else:
+            await ctx.respond(f"Reminder is canceled :)")
+
+loader.command(reminder_group)
