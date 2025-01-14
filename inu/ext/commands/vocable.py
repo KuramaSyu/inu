315c92cc444f9063b608cfdca2e01ef19c9a4601
kurama <52566613+KuramaSyu@users.noreply.github.com>
2025-01-14 21:56:16 +0100

    refactor: lb v2 -> v3

--- inu/ext/commands/vocable.py
+++ inu/ext/commands/vocable.py
@@ -9,7 +9,7 @@ import aiohttp
 import hikari
 from hikari import TextInputComponent, ButtonStyle
 import lightbulb
-from lightbulb import commands
+from lightbulb import AutocompleteContext, commands, SlashCommand, invoke
 
 import hikari
 
@@ -31,42 +31,41 @@ from core import (
     Inu, 
     Table, 
     getLogger,
-    get_context
+    get_context,
+    InuContext
 )
 
 log = getLogger(__name__)
 
-plugin = lightbulb.Plugin("Name", "Description")
+loader = lightbulb.Loader()
 bot: Inu
 
-@plugin.command
-@lightbulb.option("vocabulary-tag", "a tag which contains the vocabulary", autocomplete=True)
-@lightbulb.command("vocabulary", "train vocabulary")
-@lightbulb.implements(commands.SlashCommand)
-async def vocabulary_training(ctx: lightbulb.Context):
-    ctx.raw_options["name"] = ctx.options["vocabulary-tag"]
-    record = await get_tag_interactive(ctx)
-    if not record:
-        return await ctx.respond(f"I can't find a tag with the name `{ctx.options.name}` where you are the owner :/")
-    tag: Tag = await Tag.from_record(record, ctx.author)
-    pag = VocabularyPaginator(tag)
-    ictx = get_context(ctx.event)
-    ictx._responded = ctx._responded
-    ictx._responses = ctx._responses
-    await pag.start(ictx)
-    
-
-
-@vocabulary_training.autocomplete("vocabulary-tag")
 async def tag_name_auto_complete(
-    option: hikari.AutocompleteInteractionOption, 
-    interaction: hikari.AutocompleteInteraction
-) -> List[str]:
+    ctx: AutocompleteContext
+) -> None:
     """autocomplete for tag keys"""
-    return await TagManager.tag_name_auto_complete(option, interaction)
+    await ctx.respond(await TagManager.tag_name_auto_complete(ctx.focused, ctx.interaction)
+) 
 
-def load(inu: lightbulb.BotApp):
-    global bot
-    bot = inu
-    inu.add_plugin(plugin)
+@loader.command
+class VocabularyTraining(
+    SlashCommand,
+    name="vocabulary",
+    description="train vocabulary",
+    dm_enabled=True
+):
+    vocabulary_tag = lightbulb.string(
+        "vocabulary-tag", 
+        "a tag which contains the vocabulary", 
+        autocomplete=tag_name_auto_complete
+    )
+
+    @invoke
+    async def callback(self, _: lightbulb.Context, ctx: InuContext):
+        record = await get_tag_interactive(ctx, key=self.vocabulary_tag)
+        if not record:
+            return await ctx.respond(f"I can't find a tag with the name `{self.vocabulary_tag}` where you are the owner :/")
+        tag: Tag = await Tag.from_record(record, ctx.author)
+        pag = VocabularyPaginator(tag)
+        await pag.start(ctx)
 
