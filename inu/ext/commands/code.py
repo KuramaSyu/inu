4dcf78a043e1df104fde2a2e98d6bec9471c5ee9
kurama <paul.zenker.pz@gmail.com>
2025-01-16 19:46:09 +0100

    refactor: lb v2 -> v3

--- inu/ext/commands/code.py
+++ inu/ext/commands/code.py
@@ -1,24 +1,18 @@
-from typing import Tuple, Union, Optional, List
 import os
+from typing import Tuple
 
 import hikari
 import lightbulb
-from lightbulb.context import Context
-from lightbulb import commands
+from lightbulb import SlashCommand, invoke
 
-from core import Inu
+from core import Inu, InuContext, getLogger
 from utils import Colors, Human
 from utils.language import Multiple
-num = Human.number
-
-from core import getLogger
 
 log = getLogger(__name__)
 
-
-
-plugin = lightbulb.Plugin("Code", "Information to the ammount of code")
-
+loader = lightbulb.Loader()
+bot = Inu.instance
 
 def get_directory_size(directory: str) -> Tuple[int, int]:
     """Returns the `directory` size in bytes. and the count of all lines"""
@@ -32,7 +26,9 @@ def get_directory_size(directory: str) -> Tuple[int, int]:
                     Multiple.endswith_(
                         entry.name, 
                         [".png", ".jpg", ".gif", ".exe",
-                            ".bin", ".log", ".mp4", ".mp3", "Lavalink.jar", ".pyc", ".pyo"]
+                            ".bin", ".log", ".mp4", ".mp3", "Lavalink.jar", ".pyc", ".pyo",
+                            "LICENCE", "md", 
+                        ]
                     )
                 )
             ):
@@ -46,42 +42,40 @@ def get_directory_size(directory: str) -> Tuple[int, int]:
                 total += size
                 lines += n_lines
     except NotADirectoryError as nd:
-        log.debug(nd)
+        log.debug(f"{nd}")
         return (os.path.getsize(directory), 0)
     except PermissionError:
         # if for whatever reason we can't open the folder, return 0
         return 0, 0
     return total, lines
 
-
-
-@plugin.command
-@lightbulb.command("code", "Shows information to the ammount of code I have")
-@lightbulb.implements(commands.PrefixCommand, commands.SlashCommand)
-async def code(ctx: Context):
-    '''
-    How many bytes/code lines am I?
-    '''
-    size_in_bytes, lines = get_directory_size(f'{os.getcwd()}/inu')
-    text = (
-        f"I am written out of\n**{Human.number(int(size_in_bytes)*8)} Bits**"
-        f" (**{Human.number(round(float(size_in_bytes / 1000)))}kB | "
-        f"{Human.number(round(float(size_in_bytes / 1000 / 1000),1))}MB**)\n"
-        f"1 typical letter has a size of 1 Byte (8 Bit)\nMeans that "
-        f"I am written out of\n- **{Human.number(size_in_bytes)} letters**\n"
-        f"- or **{Human.number(lines)} lines** of code\n"
-        f"- or **{round(float(size_in_bytes/(1500 * 250)), 2)} {Human.plural_('book', float(size_in_bytes/237500), with_number=False)}**"
-        f" with 250 pages"
-    )
-    embed = hikari.Embed(
-        title="Code",
-        description=text,
-    )
-    embed.set_thumbnail(str(plugin.bot.me.avatar_url))
-    embed.color = Colors.from_name("slateblue")
-    await ctx.respond(embed=embed)
-
-
-
-def load(bot: Inu):
-    bot.add_plugin(plugin)
+@loader.command
+class CodeCommand(
+    SlashCommand,
+    name="code",
+    description="Shows information about the amount of code I have",
+    dm_enabled=False,
+    default_member_permissions=None,
+):
+    @invoke
+    async def callback(self, _: lightbulb.Context, ctx: InuContext):
+        size_in_bytes, lines = get_directory_size(f'{os.getcwd()}/inu')
+        book_count = round(float(size_in_bytes/(60 * 30 * 250)), 2)
+        text = (
+            f"I am written out of\n**{Human.number(int(size_in_bytes)*8)} Bits**"
+            f" (**{Human.number(round(float(size_in_bytes / 1000)))}kB | "
+            f"{Human.number(round(float(size_in_bytes / 1000 / 1000),1))}MB**)\n"
+            f"One typical letter has a size of 1 Byte (8 Bit)\nMeans that "
+            f"I am written out of\n- **{Human.number(size_in_bytes)} letters**\n"
+            f"- or **{Human.number(lines)} lines** of code\n"
+            f"- or **{book_count} {Human.plural_('book', book_count, with_number=False)}**"
+            f" with 250 pages (and 30 lines which each have 60 characters)"
+        )
+        # 60 chars per line, 30 lines per page, 250 pages per book
+        embed = hikari.Embed(
+            title="Code",
+            description=text,
+        )
+        embed.set_thumbnail(str(ctx.bot.me.avatar_url))
+        embed.color = Colors.from_name("slateblue")
+        await ctx.respond(embed=embed)
