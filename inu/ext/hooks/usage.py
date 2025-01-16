42619294f1a6430bcf68a558b7018370ac5e3439
kurama <paul.zenker.pz@gmail.com>
2025-01-16 20:07:35 +0100

    removed hook from usage.py

--- inu/ext/hooks/usage.py
+++ inu/ext/hooks/usage.py
@@ -7,13 +7,5 @@ from utils import InvokationStats
 LOG_USAGE = lightbulb.ExecutionStep("LOG_USAGE")
 log = getLogger(__name__)
 
+loader = lightbulb.Loader()
 
-# Define our hook to defer the command response
-@lightbulb.hook(LOG_USAGE)
-async def auto_defer_command_response(_: lightbulb.ExecutionPipeline, ctx: lightbulb.Context) -> None:
-    try:
-        log.info(f"{ctx.command_data.qualified_name} invoked by {ctx.user.display_name}")
-    except Exception:
-        log.error("Failed to defer command response", exc_info=True)
-        return
-    #await InvokationStats.add_or_sub(event.command.qualname, event.context.guild_id, 1)
