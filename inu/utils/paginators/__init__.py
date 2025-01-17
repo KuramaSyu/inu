874be23c21cf9bf512b4febb44aa0f5e394a63df
kurama <paul.zenker.pz@gmail.com>
2025-01-17 18:00:34 +0100

    Update __init__.py

--- inu/utils/paginators/__init__.py
+++ inu/utils/paginators/__init__.py
@@ -13,7 +13,7 @@ from .manga import MangaPaginator
 from .akinator_ import AkinatorSI
 from .show import ShowPaginator, MoviePaginator, tmdb_setup
 from .vocabulary import VocabularyPaginator
-from .autoroles import AutorolesView
+from .autoroles import AutorolesScreen
 from .autoroles import AutorolesViewer
 from .guilds import GuildPaginator
 
