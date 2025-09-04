6641158c0f111eae025986ac73db53b8360c7863
kurama <paul.zenker.pz@gmail.com>
2025-09-04 19:25:22 +0200

    chore: deleted .akinator_ paginator

--- inu/utils/paginators/__init__.py
+++ inu/utils/paginators/__init__.py
@@ -10,7 +10,6 @@ from .tag import TagHandler, TagCustomID, TagViewPaginator
 from .anime import AnimePaginator, AnimeCornerPaginator, AnimeCornerPaginator2
 from .anime_character import AnimeCharacterPaginator
 from .manga import MangaPaginator
-from .akinator_ import AkinatorSI
 from .show import ShowPaginator, MoviePaginator, tmdb_setup
 from .vocabulary import VocabularyPaginator
 from .autoroles import AutorolesScreen
