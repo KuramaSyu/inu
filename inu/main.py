31fdf4a47c55695b59611ed1745a03104758cc00
kurama <paul.zenker.pz@gmail.com>
2026-05-05 12:22:49 +0200

    refactor: prevent calling listeners on import

--- inu/main.py
+++ inu/main.py
@@ -32,37 +32,115 @@ from inu.ext.hooks.usage import record_command_metrics
 
 
 log = getLogger(__name__)
-log.info(f"hikari version:{hikari.__version__}")
-log.info(f"lightbulb version:{lightbulb.__version__}")
-log.info(f"lavasnek-rs version:{lavalink_rs.__version__}")
-log.info(f"hikari-miru version:{miru.__version__}")
 
 
-log.info("Create Inu")
+def create_inu() -> Inu:
+    log.info(f"hikari version:{hikari.__version__}")
+    log.info(f"lightbulb version:{lightbulb.__version__}")
+    log.info(f"lavasnek-rs version:{lavalink_rs.__version__}")
+    log.info(f"hikari-miru version:{miru.__version__}")
 
-inu = Inu()  # Instance of GatewayBot
-miru_client = miru.Client(inu, ignore_unknown_interactions=True) 
-inu._miru = miru_client
+    log.info("Create Inu")
 
-client = lightbulb.client_from_app(
-    inu, hooks=[record_command_metrics])
-# Get the registry for the default context
-registry = client.di.registry_for(lightbulb.di.Contexts.COMMAND)
-# Register our new dependency
-def get_inu_context(ctx: lightbulb.Context):
-    return get_context(ctx.interaction)
+    inu = Inu()  # Instance of GatewayBot
+    miru_client = miru.Client(inu, ignore_unknown_interactions=True)
+    inu._miru = miru_client
 
-registry.register_factory(
-    InuContext, get_inu_context
-)
+    client = lightbulb.client_from_app(
+        inu, hooks=[record_command_metrics])
+    # Get the registry for the default context
+    registry = client.di.registry_for(lightbulb.di.Contexts.COMMAND)
+    # Register our new dependency
+    def get_inu_context(ctx: lightbulb.Context):
+        return get_context(ctx.interaction)
 
-inu.client = client
-inu.subscribe(hikari.StartingEvent, client.start)
+    registry.register_factory(
+        InuContext, get_inu_context
+    )
+
+    inu.client = client
+    inu.subscribe(hikari.StartingEvent, client.start)
+
+    @inu.listen(hikari.StartingEvent)
+    async def on_ready(event: hikari.StartingEvent):
+        log.info("Connecting Database to classes", prefix="init")
+        try:
+            set_bot(inu)
+            await inu.init_db()
+            InvokationStats.init_db(inu)
+            await Reminders.init_bot(inu)
+            TagManager.init_db(inu)
+            await PollManager.init_bot(inu)
+            Urban.init_bot(inu)
+            await BoardManager.init_bot(inu)
+            MyAnimeListAIOClient.set_credentials(inu.db.bot.conf.MAL.id)
+            await tmdb_setup()
+            AutoroleManager.set_bot(inu)
+        except Exception:
+            log.critical(f"Can't connect Database to classes: {traceback.format_exc()}")
+
+        await inu.load_tasks_and_commands(["tasks", "hooks"])
+        # update bot start value
+        try:
+            table = Table("bot")
+            record = await table.select_row(["key"], ["restart_count"])
+            if not record:
+                count = 0
+            else:
+                count = int(record["value"])
+            count += 1
+            inu.restart_count = count
+            await table.upsert(where={"key": "restart_count", "value": str(count)})
+            log.info(f'Number: {(await table.select_row(["key"], ["restart_count"]))["value"]}', prefix="init")
+        except Exception:
+            log.error(traceback.format_exc())
+
+        # laod extensions
+
+    @inu.listen(hikari.StartedEvent)
+    async def on_bot_ready(event: hikari.StartedEvent):
+        async def fetch_response(number: int):
+            """Fetches a response from the numbersapi.com API"""
+            async with aiohttp.ClientSession() as session:
+                async with session.get(f"http://numbersapi.com/{number}") as resp:
+                    return (await resp.read()).decode("utf-8")
+
+        log.info("Loading start number", prefix="init")
+        table = Table("bot")
+        record = await table.select_row(["key"], ["restart_count"])
+        activity = str(record["value"]) if record else 0
+        bot_description = ""
+        try:
+            negative_answers = ["an uninteresting number", "a boring number", "we're missing a fact", "an unremarkable number"]
+            number_fact = await fetch_response(int(activity))
+            if any(answer in number_fact for answer in negative_answers):
+                new_number = int(activity) % 100
+                number_fact = (await fetch_response(new_number)).replace(str(new_number), "")
+                pattern = re.compile(str(new_number) + r"\b")
+                sub_number = pattern.sub(f"-{new_number}-", str(activity))
+                bot_description = f"{sub_number}{' Well its looking empty this time' if len(str(activity)) > len(number_fact) else number_fact}"
+            else:
+                bot_description = f"{number_fact}"
+        except Exception:
+            log.error(traceback.format_exc())
+        log.info(f"Bot is online: {bot_description}", prefix="init")
+        try:
+            await event.app.update_presence(
+                status=hikari.Status.IDLE,
+                activity=hikari.Activity(
+                    name=f"Restart Nr {activity}",
+                    state=Human.short_text(bot_description, 128),
+                    type=hikari.ActivityType.CUSTOM
+                )
+            )
+        except Exception:
+            log.error(f"failed to set presence: {traceback.format_exc()}", prefix="start")
+
+    return inu
 
 
-
-    
 def main():
+    inu = create_inu()
     stop = False
     while not stop:
         try:
@@ -103,84 +181,6 @@ def main():
 #     for record in records:
 #         inu.db.bot._prefixes[record["guild_id"]] = record["prefixes"]
 #     log.debug("Synced Prefixes", prefix="init")
-
-@inu.listen(hikari.StartingEvent)
-async def on_ready(event : hikari.StartingEvent):
-    log.info("Connecting Database to classes", prefix="init")
-    try:
-        set_bot(inu)
-        await inu.init_db()
-        InvokationStats.init_db(inu)
-        await Reminders.init_bot(inu)
-        TagManager.init_db(inu)
-        await PollManager.init_bot(inu)
-        Urban.init_bot(inu)
-        await BoardManager.init_bot(inu)
-        MyAnimeListAIOClient.set_credentials(inu.db.bot.conf.MAL.id)
-        await tmdb_setup()
-        AutoroleManager.set_bot(inu)
-    except Exception:
-        log.critical(f"Can't connect Database to classes: {traceback.format_exc()}")
-
-    await inu.load_tasks_and_commands(["tasks", "hooks"])
-    # update bot start value
-    try:
-        table = Table("bot")
-        record = await table.select_row(["key"], ["restart_count"])
-        if not record:
-            count = 0
-        else:
-            count = int(record["value"])
-        count += 1
-        inu.restart_count = count
-        await table.upsert(where={"key": "restart_count", "value": str(count)})
-        log.info(f'Number: {(await table.select_row(["key"], ["restart_count"]))["value"]}', prefix="init")
-    except Exception:
-        log.error(traceback.format_exc())
-        
-    # laod extensions
-    
-
-
-@inu.listen(hikari.StartedEvent)
-async def on_bot_ready(event : hikari.StartedEvent):
-    async def fetch_response(number: int):
-        """Fetches a response from the numbersapi.com API"""
-        async with aiohttp.ClientSession() as session:
-            async with session.get(f"http://numbersapi.com/{number}") as resp:
-                return (await resp.read()).decode("utf-8")
-            
-    log.info("Loading start number", prefix="init")
-    table = Table("bot")
-    record = await table.select_row(["key"], ["restart_count"])
-    activity = str(record["value"]) if record else 0
-    bot_description = ""
-    try:
-        negative_answers = ["an uninteresting number", "a boring number", "we're missing a fact", "an unremarkable number"]
-        number_fact = await fetch_response(int(activity))
-        if any(answer in number_fact for answer in negative_answers):
-            new_number = int(activity) % 100
-            number_fact = (await fetch_response(new_number)).replace(str(new_number), "")
-            pattern = re.compile(str(new_number) + r"\b")
-            sub_number = pattern.sub(f"-{new_number}-", str(activity))
-            bot_description = f"{sub_number}{' Well its looking empty this time' if len(str(activity)) > len(number_fact) else number_fact}"
-        else:
-            bot_description = f"{number_fact}"
-    except Exception:
-        log.error(traceback.format_exc())
-    log.info(f"Bot is online: {bot_description}", prefix="init")
-    try:
-        await event.app.update_presence(
-            status=hikari.Status.IDLE, 
-            activity=hikari.Activity(
-                name=f"Restart Nr {activity}",
-                state=Human.short_text(bot_description, 128),
-                type=hikari.ActivityType.CUSTOM
-            )
-        )
-    except Exception:
-        log.error(f"failed to set presence: {traceback.format_exc()}", prefix="start")
-
 
 if __name__ == "__main__":
     # if os.name != "nt":
